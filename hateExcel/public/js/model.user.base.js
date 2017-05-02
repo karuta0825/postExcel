79361660b6aa1743fc4360cd5795d7d3b31740e2
karuta0825 <karuta0825@users.noreply.github.com>
2017-05-02 15:30:14 +0900

    modify the way to update.

--- hateExcel/public/js/model.user.base.js
+++ hateExcel/public/js/model.user.base.js
@@ -23,9 +23,26 @@
     return _cache;
   };
 
+  /**
+   * customerテーブルをアップデート
+   * @param  {[type]} data
+   * @param  {[type]} condition
+   * @return {[type]}
+   */
+  update = function ( data, condition ) {
+
+    customer.db.update('/update', {
+      data      : data,
+      condition : condition,
+      table     : 'customers'
+    });
+
+  };
+
   cms.model.userBaseInfo = {
     fetch : fetch,
-    getCache : getCache
-  }
+    getCache : getCache,
+    update : update
+  };
 
 }( jQuery, customer ));
