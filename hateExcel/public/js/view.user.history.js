16a6c30b7fbeb19bd7234a270a8e1684e3767ac5
karuta <meta7p.takapon@gmail.com>
2017-05-03 14:01:22 +0900

    履歴のviewをひとまず作成。

---
+++ hateExcel/public/js/view.user.history.js
@@ -0,0 +1,32 @@
+
+/**
+ * 履歴情報
+ */
+
+( function ( $, cms ) {
+
+  var
+    historyView
+  , elements = {
+
+    }
+  , _drawTable
+  , initModule
+  ;
+
+  _drawTable = function ( data ) {
+
+  };
+
+
+  initModule = function () {
+
+  };
+
+  // to public
+  cms.view.userHistory = {
+    initModule : initModule
+  };
+
+
+}( jQuery, customer ));
