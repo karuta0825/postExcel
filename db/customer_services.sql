6d771d044490db5f0aca11177b493fe5110340e8
karuta <meta7p.takapon@gmail.com>
2017-05-04 12:49:29 +0900

    table更新

--- db/customer_services.sql
+++ db/customer_services.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
 --
--- Host: localhost    Database: customer
+-- Host: 127.0.0.1    Database: customer
 -- ------------------------------------------------------
--- Server version	5.7.9-log
+-- Server version	5.7.12
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -23,9 +23,10 @@ DROP TABLE IF EXISTS `services`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `services` (
+  `order` int(3) NOT NULL,
   `service_id` varchar(2) NOT NULL,
   `service_name` varchar(45) NOT NULL,
-  PRIMARY KEY (`service_id`),
+  PRIMARY KEY (`order`,`service_id`),
   UNIQUE KEY `service_id_UNIQUE` (`service_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -36,7 +37,7 @@ CREATE TABLE `services` (
 
 LOCK TABLES `services` WRITE;
 /*!40000 ALTER TABLE `services` DISABLE KEYS */;
-INSERT INTO `services` VALUES ('C1','C1サービス'),('C2','C2サービス'),('C3','C3サービス'),('C4','C4サービス'),('K1','基本サービス'),('S2','S2サービス'),('S3','S3サービス'),('S9','S9サービス'),('SC','SCサービス'),('SE','SEサービス'),('SH','SHサービス'),('SI','SIサービス'),('SJ','SJサービス'),('SL','SLサービス'),('SM','SMサービス'),('SO','SOサービス'),('U1','U1サービス'),('U2','U2サービス'),('U3','U3サービス'),('U4','U4サービス'),('U5','U5サービス'),('U6','U6サービス'),('U7','U7サービス'),('U8','U8サービス'),('U9','U9サービス'),('UA','UAサービス'),('UB','UBサービス'),('UC','UCサービス'),('UD','UDサービス'),('UE','UEサービス'),('UF','UFサービス'),('UG','UGサービス'),('UH','UHサービス'),('UI','UIサービス');
+INSERT INTO `services` VALUES (1,'K1','基本サービス'),(2,'U1','U1サービス'),(3,'U2','U2サービス'),(4,'U3','U3サービス'),(5,'U4','U4サービス'),(6,'U5','U5サービス'),(7,'U6','U6サービス'),(8,'U7','U7サービス'),(9,'U8','U8サービス'),(10,'U9','U9サービス'),(11,'UA','UAサービス'),(12,'UB','UBサービス'),(13,'UC','UCサービス'),(14,'UD','UDサービス'),(15,'UE','UEサービス'),(16,'UF','UFサービス'),(17,'UG','UGサービス'),(18,'UH','UHサービス'),(19,'UI','UIサービス'),(20,'C1','C1サービス'),(21,'C2','C2サービス'),(22,'C3','C3サービス'),(23,'C4','C4サービス'),(24,'S2','S2サービス'),(25,'S3','S3サービス'),(26,'S9','S9サービス'),(27,'SC','SCサービス'),(28,'SE','SEサービス'),(29,'SH','SHサービス'),(30,'SI','SIサービス'),(31,'SJ','SJサービス'),(32,'SL','SLサービス'),(33,'SM','SMサービス'),(34,'SO','SOサービス');
 /*!40000 ALTER TABLE `services` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -49,4 +50,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-03 20:59:37
+-- Dump completed on 2017-05-04 12:49:15
