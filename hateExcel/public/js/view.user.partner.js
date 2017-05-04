563365d73749fe0d5cbeb7ad1b5429068bef1916
karuta <meta7p.takapon@gmail.com>
2017-05-04 12:45:40 +0900

    パートナータブ修正

--- hateExcel/public/js/view.user.partner.js
+++ hateExcel/public/js/view.user.partner.js
@@ -14,7 +14,7 @@
       },
       'input' : {
         'pid'           : '.pid',
-        'company'       : '.company',
+        'name'          : '.name',
         'postal_cd'     : '.postal_cd',
         'address'       : '.address',
         'se_affliation' : '.se_affliation',
@@ -37,7 +37,8 @@
   , save
   , edit
   , reset
-  , inputData
+  , setInfo
+  , clear
   , initModule
   ;
 
@@ -51,11 +52,27 @@
     partnerView.get('btn__cancel').addClass('is-hidden');
     partnerView.get('btn__save').addClass('is-hidden');
 
-    // モデルのデータを挿入
 
   };
 
-  save = function () {};
+  save = function () {
+
+    // update
+    customer.model.userPartner.update( getViewInfo(), setInfo );
+
+    // 編集不可
+    _.each( partnerView.get('input'), function (val, key){
+      val.find('.item-value').removeClass('is-edit');
+      val.find('.item-value').prop('disabled', true);
+    });
+
+    // ボタン状態制御
+    partnerView.get('btn__edit').removeClass('is-hidden');
+    partnerView.get('btn__cancel').addClass('is-hidden');
+    partnerView.get('btn__save').addClass('is-hidden');
+
+
+  };
 
   edit = function () {
     _.each( partnerView.get('input'), function (val, key){
@@ -69,29 +86,77 @@
 
   };
 
-  reset = function () {};
+  reset = function () {
+    customer.model.userPartner.getCache( setInfo );
+  };
 
-  getViewData = function () {
+  setInfo = function ( data ) {
+
+    partnerView.get('input__pid'          ).find('.item-value').val( data.pid  );
+    partnerView.get('input__name'         ).find('.item-value').val( data.name );
+    partnerView.get('input__postal_cd'    ).find('.item-value').val( data.postal_cd);
+    partnerView.get('input__address'      ).find('.item-value').val( data.address);
+    partnerView.get('input__se_affliation').find('.item-value').val( data.se_affliation);
+    partnerView.get('input__se_name'      ).find('.item-value').val( data.se_name );
+    partnerView.get('input__se_tel'       ).find('.item-value').val( data.se_tel );
+    partnerView.get('input__se_fax'       ).find('.item-value').val( data.se_fax);
+    partnerView.get('input__se_email'     ).find('.item-value').val( data.se_email);
+    partnerView.get('input__sa_affliation').find('.item-value').val( data.sa_affliation);
+    partnerView.get('input__sa_name'      ).find('.item-value').val( data.sa_name );
+    partnerView.get('input__sa_tel'       ).find('.item-value').val( data.sa_tel );
+    partnerView.get('input__sa_fax'       ).find('.item-value').val( data.sa_fax );
+    partnerView.get('input__sa_email'     ).find('.item-value').val( data.sa_email );
+    partnerView.get('input__em_company'   ).find('.item-value').val( data.em_company );
+    partnerView.get('input__em_name'      ).find('.item-value').val( data.em_name );
+    partnerView.get('input__em_tel'       ).find('.item-value').val( data.em_tel );
+    partnerView.get('input__em_email'     ).find('.item-value').val( data.em_email) ;
+
+  };
+
+  getViewInfo = function () {
     return {
-      'pid'           : partnerView.get('input__pid'          ).find('.item-value').val(),
-      'company'       : partnerView.get('input__company'      ).find('.item-value').val(),
-      'postal_cd'     : partnerView.get('input__postal_cd'    ).find('.item-value').val(),
-      'address'       : partnerView.get('input__address'      ).find('.item-value').val(),
-      'se_affliation' : partnerView.get('input__se_affliation').find('.item-value').val(),
-      'se_name'       : partnerView.get('input__se_name'      ).find('.item-value').val(),
-      'se_tel'        : partnerView.get('input__se_tel'       ).find('.item-value').val(),
-      'se_fax'        : partnerView.get('input__se_fax'       ).find('.item-value').val(),
-      'se_email'      : partnerView.get('input__se_email'     ).find('.item-value').val(),
-      'sa_affliation' : partnerView.get('input__sa_affliation').find('.item-value').val(),
-      'sa_name'       : partnerView.get('input__sa_name'      ).find('.item-value').val(),
-      'sa_tel'        : partnerView.get('input__sa_tel'       ).find('.item-value').val(),
-      'sa_fax'        : partnerView.get('input__sa_fax'       ).find('.item-value').val(),
-      'sa_email'      : partnerView.get('input__sa_email'     ).find('.item-value').val(),
-      'em_company'    : partnerView.get('input__em_company'   ).find('.item-value').val(),
-      'em_name'       : partnerView.get('input__em_name'      ).find('.item-value').val(),
-      'em_tel'        : partnerView.get('input__em_tel'       ).find('.item-value').val(),
-      'em_email'      : partnerView.get('input__em_email'     ).find('.item-value').val()
+      'pid'           : Number(partnerView.get('input__pid'       ).find('.item-value').val()),
+      'name'          : partnerView.get('input__name'             ).find('.item-value').val(),
+      'postal_cd'     : Number(partnerView.get('input__postal_cd' ).find('.item-value').val()),
+      'address'       : partnerView.get('input__address'          ).find('.item-value').val(),
+      'se_affliation' : partnerView.get('input__se_affliation'    ).find('.item-value').val(),
+      'se_name'       : partnerView.get('input__se_name'          ).find('.item-value').val(),
+      'se_tel'        : partnerView.get('input__se_tel'           ).find('.item-value').val(),
+      'se_fax'        : partnerView.get('input__se_fax'           ).find('.item-value').val(),
+      'se_email'      : partnerView.get('input__se_email'         ).find('.item-value').val(),
+      'sa_affliation' : partnerView.get('input__sa_affliation'    ).find('.item-value').val(),
+      'sa_name'       : partnerView.get('input__sa_name'          ).find('.item-value').val(),
+      'sa_tel'        : partnerView.get('input__sa_tel'           ).find('.item-value').val(),
+      'sa_fax'        : partnerView.get('input__sa_fax'           ).find('.item-value').val(),
+      'sa_email'      : partnerView.get('input__sa_email'         ).find('.item-value').val(),
+      'em_company'    : partnerView.get('input__em_company'       ).find('.item-value').val(),
+      'em_name'       : partnerView.get('input__em_name'          ).find('.item-value').val(),
+      'em_tel'        : partnerView.get('input__em_tel'           ).find('.item-value').val(),
+      'em_email'      : partnerView.get('input__em_email'         ).find('.item-value').val()
     }
+  };
+
+  clear = function () {
+    setInfo({
+      'pid'           : '',
+      'name'          : '',
+      'postal_cd'     : '',
+      'address'       : '',
+      'se_affliation' : '',
+      'se_name'       : '',
+      'se_tel'        : '',
+      'se_fax'        : '',
+      'se_email'      : '',
+      'sa_affliation' : '',
+      'sa_name'       : '',
+      'sa_tel'        : '',
+      'sa_fax'        : '',
+      'sa_email'      : '',
+      'em_company'    : '',
+      'em_name'       : '',
+      'em_tel'        : '',
+      'em_email'      : ''
+    })
   };
 
   initModule = function () {
@@ -105,15 +170,19 @@
 
     // Event定義
     partnerView.addListener({
-      'click btn__edit' : edit,
-      'click btn__cancel' : cancel
+      'click btn__edit'   : edit,
+      'click btn__cancel' : cancel,
+      'click btn__save'   : save
     });
 
   };
 
   // to public
   cms.view.userPartner = {
-    initModule : initModule,
+    initModule  : initModule,
+    setInfo     : setInfo,
+    clear       : clear,
+    getViewInfo : getViewInfo,
     get : function () { return partnerView; }
   }
 
