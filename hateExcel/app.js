89bc6dfdaee39593c28fc8d7aabe952717c0f7e0
karuta0825 <karuta0825@users.noreply.github.com>
2017-05-09 17:40:06 +0900

    port戻す

--- hateExcel/app.js
+++ hateExcel/app.js
@@ -14,7 +14,7 @@ var datas = require('./models/datas');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 8000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
