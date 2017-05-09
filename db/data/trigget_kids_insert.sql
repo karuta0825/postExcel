2c2e06f231daf25cc0a6f2923347a45489403fab
karuta0825 <karuta0825@users.noreply.github.com>
2017-05-09 13:09:33 +0900

    システムタイプ別分岐処理を追加

--- db/data/trigget_kids_insert.sql
+++ db/data/trigget_kids_insert.sql
@@ -5,12 +5,24 @@ DELIMITER $$
 
 CREATE TRIGGER kids_insert AFTER INSERT ON kids FOR EACH ROW
  BEGIN
-   INSERT INTO licenses (kid) values(new.kid);
-   INSERT INTO customers (kid) values( new.kid );
-   INSERT INTO clients ( kid, client_id, client_pass, update_on, is_admin ) values ( new.kid, concat( new.userkey, '0999'), concat( new.userkey, '0999'), new.create_on, 1, new.create_user_id );
-   INSERT INTO clients ( kid, client_id, client_pass, update_on, is_admin ) values ( new.kid, concat( new.userkey, '1999'), concat( new.userkey, '1999'), new.create_on, 1, new.create_user_id );
-   INSERT INTO partners (kid) values(new.kid);
-   INSERT INTO historys (kid, type, create_on, create_user_id ) values( new.kid, '新規', new.create_on, new.create_user_id );
+
+	
+	INSERT INTO licenses (kid) values(new.kid);
+	INSERT INTO customers (kid) values( new.kid );
+   
+   
+	INSERT INTO partners (kid) values(new.kid);
+	INSERT INTO historys (kid, type, create_on, create_user_id ) values( new.kid, '新規', new.create_on, new.create_user_id );
+   
+   
+	if new.userkey != '' then
+		INSERT INTO clients ( kid, client_id, client_pass, update_on, is_admin, create_user_id ) values ( new.kid, concat( new.userkey, '0999'), concat( new.userkey, '0999'), new.create_on, 1, new.create_user_id );
+		INSERT INTO clients ( kid, client_id, client_pass, update_on, is_admin, create_user_id ) values ( new.kid, concat( new.userkey, '1999'), concat( new.userkey, '1999'), new.create_on, 1, new.create_user_id );
+	end if;
+
+
+    
+   
  END;
 $$
 
