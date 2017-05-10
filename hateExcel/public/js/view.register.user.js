ddfeb3d0d60a0cadc2203485925e6aba7f69f255
karuta <meta7p.takapon@gmail.com>
2017-05-11 07:45:22 +0900

    ファイルアップロード基本機能実装。

--- hateExcel/public/js/view.register.user.js
+++ hateExcel/public/js/view.register.user.js
@@ -6,33 +6,96 @@
 
   var
   // member
-    jqueryMap = {}
-  , _setJqueryMap
+    registerView
+  , elements = {
+      'upload' : '.upload',
+      'btn' : {
+        'upload' : '.btn--upload'
+      }
+    }
   , _onClickUpload
+  , _selectFile
+  , _dragOver
   , initModule
   ;
-
-
-  /**
-   * DOMキャッシュ設定
-   */
-  _setJqueryMap = function () {
-    jqueryMap.btnUpload = $('.btn--upload');
-  };
 
   _onClickUpload = function () {
     console.log('upload');
   };
 
+  _selectFile = function ( evt )  {
+
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    var
+      files = evt.dataTransfer.files
+    , r = new FileReader()
+    ;
+
+    r.readAsText(files[0]);
+
+    r.onload = function ( e ) {
+
+      var
+        line_code = r.result.indexOf('\r') === -1 ? '\n' : '\r\n'
+      , list_oneline = r.result.split(line_code)
+      , map_result = {}
+      ;
+
+      // 該当の行のみ抽出
+      list_oneline = _.select( list_oneline, function (val, key) {
+        if ( val !== '' ) {
+          return val;
+        }
+      });
+
+      // 連想配列作成
+      _.each( list_oneline, function (v,k) {
+        var delimiter_position, key, val;
+
+        delimiter_position = v.indexOf(':');
+        key = v.slice(0,delimiter_position);
+        val = v.slice(delimiter_position+1);
+
+        map_result[key] = val;
+
+      });
+
+      console.log(map_result);
+
+      // 入力チェック
+
+      // ボタン状態制御
+      registerView.get('btn__upload').prop('disabled', false);
+
+    }
+
+  };
+
+  _dragOver = function ( evt ) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.dataTransfer.dropEffect = 'copy';
+  };
+
+
   initModule = function () {
+
+    // デフォルトイベントを抑止
+    $.event.props.push('dataTransfer');
 
     // コンテンツの挿入
     $('.main-contents--reg-usr').append( customer.db.getHtml('register.user.html'));
 
-    // DOMキャッシュ作成
-    _setJqueryMap();
+    registerView = new Controller('.main-contents--reg-usr');
+    registerView.initElement( elements );
 
-    jqueryMap.btnUpload.on( 'click', _onClickUpload );
+    registerView.addListener({
+      'click btn__upload' : _onClickUpload,
+      'dragover upload' : _dragOver,
+      'drop upload' : _selectFile
+    });
 
   };
 
