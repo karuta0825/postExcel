f1f0644227ef26d67120f7539c3fd9c19177dc2f
karuta <meta7p.takapon@gmail.com>
2017-05-10 21:49:27 +0900

    デリートトリガー追加

---
+++ db/data/trigger_kids_delete.sql
@@ -0,0 +1,18 @@
+#drop trigger kids_delete;
+
+use customer;
+DELIMITER $$
+
+CREATE TRIGGER kids_delete AFTER DELETE ON kids FOR EACH ROW
+ BEGIN
+
+	delete from licenses where kid = old.kid;
+    delete from customers where kid = old.kid;
+    delete from partners where kid = old.kid;    
+    delete from clients where kid = old.kid;
+    delete from historsy where kid = old.kid;
+   
+ END;
+$$
+
+DELIMITER ;kids_delete
