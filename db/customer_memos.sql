7c7d3009048720ea26a2f67165a5c2b482599e59
karuta <meta7p.takapon@gmail.com>
2017-05-22 08:07:14 +0900

    テーブル更新

--- db/customer_memos.sql
+++ db/customer_memos.sql
@@ -26,14 +26,14 @@ CREATE TABLE `memos` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `kid` varchar(9) NOT NULL,
   `title` varchar(45) NOT NULL,
-  `priority` int(1) NOT NULL,
-  `message` varchar(255) NOT NULL,
+  `priority_id` int(1) NOT NULL,
+  `message` varchar(300) NOT NULL,
   `create_on` datetime NOT NULL,
   `create_user_id` int(1) NOT NULL,
   `update_on` datetime DEFAULT NULL,
   `update_user_id` int(1) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -42,7 +42,7 @@ CREATE TABLE `memos` (
 
 LOCK TABLES `memos` WRITE;
 /*!40000 ALTER TABLE `memos` DISABLE KEYS */;
-INSERT INTO `memos` VALUES (1,'KID77891','タイトル',1,'KID77891のメッセージです。今日は非常に眠たい日です。エンジニアは細かいところに注意がいくものですが、僕は適当な人間です。','2017-05-12 00:00:00',1,NULL,NULL);
+INSERT INTO `memos` VALUES (1,'KID77891','タイトル',1,'KID77891のメッセージです。今日は非常に眠たい日です。エンジニアは細かいところに注意がいくものですが、僕は適当な人間です。','2017-05-12 00:00:00',1,NULL,2),(2,'KID77891','タイトル２',2,'KID744','2017-05-20 12:32:23',1,NULL,NULL),(3,'KID92468','テスト',3,' // まずキャッシュを見に行くけど最初は\'number\'が存在しないので、カスタムデータ属性を見に行って\"01\"を取得する\n// そしてついでにデータをキャッシュする\n$(\'#fruit\').data(\'number\')\n\n// attr()を使ってカスタムデータ属性を変えてみる\n$(\'#fruit\').attr(\'data-number\', \'02\')\n\n// キャッシュを見に行くと前回取得時に作ったキャッシュがあるので、そこから \"01\" を取得する\n$(\'#fruit\').data(\'number\')','2017-05-21 11:34:21',1,NULL,NULL),(4,'KID98370','test',2,'','2017-05-21 11:54:23',1,NULL,NULL),(5,'KID98370','test',2,'テスト内容あああああああああああああああああああああ\n３００文字までしか保証しておりません。','2017-05-21 11:54:57',1,NULL,NULL),(6,'KID92468','test_update',2,'優先度を重要に変更しました。\n赤さんの変更しました。','2017-05-21 12:01:04',1,'2017-05-21 18:27:16',2),(7,'KID92468','テスト',3,'青鬼が変更しました。','2017-05-21 12:03:47',1,'2017-05-21 19:55:34',2),(8,'KID92468','テスト',1,'優先度を緊急に変更','2017-05-21 12:06:32',1,NULL,NULL),(9,'KID92468','テスト',1,'優先度を緊急に変更。あああああああああああああ\n\n緊急な内容ですよね。aaaaaaffaf','2017-05-21 12:07:47',1,NULL,NULL),(15,'KID60414','テスト１',3,'テスト１','2017-05-21 12:20:20',1,NULL,NULL),(17,'KID60414','テスト３',2,'優先度の変更','2017-05-21 12:26:07',1,NULL,NULL),(19,'KID10177','テスト１',2,'テスト','2017-05-21 13:27:13',1,NULL,NULL),(20,'KID92468','新規作成',1,'優先度変更','2017-05-21 14:51:36',1,NULL,NULL),(21,'KID92468','メモ',1,'作成しました。修正しました。','2017-05-21 18:22:39',2,'2017-05-21 18:26:17',NULL);
 /*!40000 ALTER TABLE `memos` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -55,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-05-16 21:03:56
+-- Dump completed on 2017-05-22  8:06:12
