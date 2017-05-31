2a897a86ddc38120f52f5aee859a4a029536084f
karuta <meta7p.takapon@gmail.com>
2017-05-31 21:15:19 +0900

    開始日も自動生成

--- hateExcel/models/datas.js
+++ hateExcel/models/datas.js
@@ -377,7 +377,9 @@ var makeFenicsAccount = function ( input_map, callback ) {
     fenics_account['password']   = results[0];
     fenics_account['fenics_ip']  = results[1];
     fenics_account['fenics_key'] = input_map.fenics_key;
+    fenics_account['start_on']   = new Date();
     fenics_account['create_on']  = new Date();
+
     // check
     // console.log( fenics_account );
 
