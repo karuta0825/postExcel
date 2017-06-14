8be6260bf5bee0cf8d4628d7177b9efbab9463b4
karuta0825 <magune3g@gmail.com>
2017-06-15 07:24:11 +0900

    集計スクリプト作成

--- db/data/get_user_every_month.sql
+++ db/data/get_user_every_month.sql
@@ -1,8 +1,29 @@
-
+use customer;
+(
 SELECT
-	DATE_FORMAT(create_on, "%Y/%m") as month,
-	count(kid) as number
+	'ユーザー作成' as item_name,
+	count(kid) as new_user_number,
+    DATE_FORMAT(create_on, "%Y/%m") as month
 FROM
-    customer.kids
+    kids
 GROUP BY
-    DATE_FORMAT(create_on, '%Y/%m');
+    DATE_FORMAT(create_on, '%Y/%m')
+)
+union all
+(
+select 
+ item_name,
+ sum(diff),
+ DATE_FORMAT(create_on, "%Y/%m") as month
+from (
+SELECT
+	item_name,
+	`after` - `before` as diff,
+    create_on
+FROM historys
+where item_name = 'クライアント数' or item_name = '端末台数'
+) T
+GROUP BY
+    DATE_FORMAT(T.create_on, '%Y/%m'), T.item_name
+)
+order by month;
