825d834220eee3799b7880677e78efb009a4c73c
karuta0825 <karuta0825@users.noreply.github.com>
2017-06-16 19:07:00 +0900

    グラフ表示用月別データ取得スクリプト作成

--- db/data/get_user_every_month.sql
+++ db/data/get_user_every_month.sql
@@ -1,8 +1,67 @@
-
+use customer;
+select 
+ month,
+ sum(user) as user,
+ sum(client) as client,
+ sum(pc) as pc
+from ( 
 SELECT
-	DATE_FORMAT(create_on, "%Y/%m") as month,
-	count(kid) as number
+	month,
+    CASE item_name
+        WHEN 'ユーザー作成' THEN new_user_number
+        ELSE NULL
+    END AS `user`,
+    CASE item_name
+        WHEN 'クライアント数' THEN new_user_number
+        ELSE NULL
+    END AS `client`,
+    CASE item_name
+        WHEN '端末台数' THEN new_user_number
+        ELSE NULL
+    END AS `pc`
 FROM
-    customer.kids
-GROUP BY
-    DATE_FORMAT(create_on, '%Y/%m');
+    ((SELECT 
+        'ユーザー作成' AS item_name,
+            COUNT(kid) AS new_user_number,
+            DATE_FORMAT(create_on, '%Y/%m') AS month
+    FROM
+        kids
+    GROUP BY DATE_FORMAT(create_on, '%Y/%m')) UNION ALL (SELECT 
+        item_name,
+            SUM(diff),
+            DATE_FORMAT(create_on, '%Y/%m') AS month
+    FROM
+        (SELECT 
+        item_name, `after` - `before` AS diff, create_on
+    FROM
+        historys
+    WHERE
+        item_name = 'クライアント数'
+            OR item_name = '端末台数') AS T
+    GROUP BY DATE_FORMAT(T.create_on, '%Y/%m') , T.item_name)) AS tmp
+    ) as a group by month
+    
+    
+    
+# part2
+select 
+	DATE_FORMAT(create_on, '%Y/%m') as month,
+    sum(case item_name when 'ユーザー作成' THEN val else null end ) as user,
+    sum(case item_name when 'クライアント数' THEN val else null end ) as client,
+    sum(case item_name when '端末台数' THEN val else null end ) as pc
+from ((
+    SELECT 
+        'ユーザー作成' AS item_name,
+		1 as val,
+        create_on
+    FROM
+        kids
+	) UNION ALL (
+	SELECT 
+        item_name, `after` - `before` AS diff, create_on
+    FROM
+        historys
+    WHERE
+        item_name = 'クライアント数' OR item_name = '端末台数'
+	)) AS T
+    GROUP BY DATE_FORMAT(create_on, '%Y/%m');
