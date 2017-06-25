a2bf7679f7d6a7d7a323053cc4a19702ce87d5d1
karuta0825 <magune3g@gmail.com>
2017-06-25 17:46:21 +0900

    グラフのアクション部分と本体部分のデザイン修正

--- hateExcel/public/js/view.home.graph.js
+++ hateExcel/public/js/view.home.graph.js
@@ -8,58 +8,78 @@
   // member
     viewGraph
   , elements = {
-      'lm' : '.article__body .lm',
-      'es' : '.article__body .es'
+      'LM' : '.article__body .lm',
+      'ES' : '.article__body .es'
     }
   , timer = false
-  , resize_chart
-  , resize_options
-  , resize_data
-  , resize_chart_es
-  , resize_options_es
-  , resize_data_es
+  , resize_chart = {}
+  , resize_options = {}
+  , resize_data = {}
   // public method
   , initModule
   , draw
+  , redraw
   ;
 
-  draw = function ( list_data, list_header ) {
-
-    list_data.unshift(list_header);
+  draw = function () {
 
     google.charts.setOnLoadCallback(
       function() {
 
         // LM
-        resize_data = google.visualization.arrayToDataTable(list_data);
-
-        resize_options = {
-          title: 'LM',
-          hAxis: {title: '月'},
-          legend:{position:'bottom'}
-        };
-
-        resize_chart = new google.visualization.ColumnChart(viewGraph.get('lm').get(0));
-        resize_chart.draw(resize_data, resize_options);
+        draws('LM')
 
         // ES
-        resize_data_es = google.visualization.arrayToDataTable(list_data);
-
-        resize_options_es = {
-          title: 'ES',
-          hAxis: {title: '月'},
-          legend:{position:'bottom'}
-        };
-
-        resize_chart_es = new google.visualization.ColumnChart(viewGraph.get('es').get(0));
-        resize_chart_es.draw(resize_data_es, resize_options_es);
-
+        draws('ES')
 
       }
     );
 
   };
 
+  draws = function (version) {
+
+    var list_data = _.where( customer.db.select('/select',{'table' : 'get_add_info_in_each_month'}), {version: version});
+
+    list_data = _.map(list_data, function (v,k) {
+      delete v.version;
+      return v;
+    });
+
+    list_data = _.map( list_data, function ( v,k ) {
+      return _.values(v);
+    });
+
+    list_data.unshift(['','ユーザ','クライア ント', 'PC']);
+
+    resize_data[version] = google.visualization.arrayToDataTable(list_data);
+
+    resize_options[version] = {
+      title: version,
+      hAxis: {title: '月'},
+      legend:{position:'bottom'}
+    };
+
+    resize_chart[version] = new google.visualization.ColumnChart(viewGraph.get(version).get(0));
+    resize_chart[version].draw(resize_data[version], resize_options[version]);
+
+  };
+
+  redraw = function () {
+    var model = _.where( customer.db.select('/select',{'table' : 'get_add_info_in_each_month'}), {version:'ES'});
+
+    model = _.map(model, function (v,k) {
+      delete v.version;
+      return v;
+    });
+
+    model = _.map( model, function ( v,k ) {
+      return _.values(v);
+    });
+
+    draw( model, ['','ユーザ','クライア ント', 'PC']);
+
+  };
 
   initModule = function () {
 
@@ -98,7 +118,11 @@
 
   // to public
   cms.view.homeGraph = {
-    initModule : initModule
+    initModule : initModule,
+    empty : function () { viewGraph.get('lm').empty(); },
+    get : function () { resize_chart.draw(resize_data, resize_options); },
+    redraw : redraw
+ 
   };
 
 }( jQuery, customer ));
