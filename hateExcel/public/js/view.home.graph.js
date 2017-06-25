d5cbce97eca402109376445200116a857fa93a88
karuta0825 <magune3g@gmail.com>
2017-06-25 13:47:00 +0900

    グラフ２列表示

--- hateExcel/public/js/view.home.graph.js
+++ hateExcel/public/js/view.home.graph.js
@@ -15,8 +15,13 @@
   , resize_chart
   , resize_options
   , resize_data
+  , resize_chart_es
+  , resize_options_es
+  , resize_data_es
   // public method
   , initModule
+  , draw
+  ;
 
   draw = function ( list_data, list_header ) {
 
@@ -24,6 +29,8 @@
 
     google.charts.setOnLoadCallback(
       function() {
+
+        // LM
         resize_data = google.visualization.arrayToDataTable(list_data);
 
         resize_options = {
@@ -34,27 +41,20 @@
 
         resize_chart = new google.visualization.ColumnChart(viewGraph.get('lm').get(0));
         resize_chart.draw(resize_data, resize_options);
-      }
-    );
 
-  };
+        // ES
+        resize_data_es = google.visualization.arrayToDataTable(list_data);
 
-  drawES = function ( list_data, list_header ) {
-
-    list_data.unshift(list_header);
-
-    google.charts.setOnLoadCallback(
-      function() {
-        resize_data = google.visualization.arrayToDataTable(list_data);
-
-        resize_options = {
-          title: 'LM',
+        resize_options_es = {
+          title: 'ES',
           hAxis: {title: '月'},
           legend:{position:'bottom'}
         };
 
-        resize_chart = new google.visualization.ColumnChart(viewGraph.get('es').get(0));
-        resize_chart.draw(resize_data, resize_options);
+        resize_chart_es = new google.visualization.ColumnChart(viewGraph.get('es').get(0));
+        resize_chart_es.draw(resize_data_es, resize_options_es);
+
+
       }
     );
 
@@ -80,8 +80,7 @@
       return _.values(v);
     });
 
-    draw( model, ['','ユーザ','クライアント', 'PC']);
-    // drawES( model, ['','ユーザ','クライアント', 'PC']);
+    draw( model, ['','ユーザ','クライア ント', 'PC']);
 
     // windowリサイズ時にグラフ長さ調整
     $(window).resize(function() {
