04abc48053fcb7a063ca03418bcc4683ebbd00fa
karuta0825 <magune3g@gmail.com>
2017-07-02 11:45:35 +0900

    端末追加はテーブルで持たせるため、削除しない

--- hateExcel/public/js/model.kids.js
+++ hateExcel/public/js/model.kids.js
@@ -174,7 +174,6 @@
 
     // クライアント・PC数はテーブルにないため削除
     delete update_data.client_number;
-    delete update_data.number_pc;
 
     // updateする対象が存在する場合
     if ( _.keys(update_data).length > 0 ) {
