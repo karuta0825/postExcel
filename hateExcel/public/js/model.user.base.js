3e40b266f7fbc25d6782dd1415efa89b0eb31024
karuta0825 <magune3g@gmail.com>
2017-07-02 17:26:42 +0900

    null時の対応

--- hateExcel/public/js/model.user.base.js
+++ hateExcel/public/js/model.user.base.js
@@ -45,7 +45,7 @@
     _cache = _.clone( customer.model.kids.find( {'kid' : kid } )[0] );
 
     // 端末IDの
-    _cache.start_id = Number(_cache.range_id.split('-')[0]);
+    _cache.start_id = _cache.range_id && Number(_cache.range_id.split('-')[0]) || '';
     delete _cache.range_id
 
     if ( typeof callback === 'function' ) {
