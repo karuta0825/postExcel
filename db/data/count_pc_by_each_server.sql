6e28c5cbe0bd6752a8e922a147a6b467723eefc7
karuta0825 <magune3g@gmail.com>
2017-07-12 20:04:09 +0900

    お知らせの種別のフィルター機能を実装

--- db/data/count_pc_by_each_server.sql
+++ db/data/count_pc_by_each_server.sql
@@ -1,17 +1,35 @@
 use customer;
-select
-	GRP.server,
+SELECT 
+    GRP.server,
     GRP.pc_number,
-    80 - GRP.pc_number as available_number,
+    S.capacity - GRP.pc_number AS available_number,
     S.version
-from 
-(SELECT 
-    server, 
-    SUM(number_pc) as pc_number
 FROM
-    kids K
-WHERE
-    server != ''
-GROUP BY server) GRP
-left join servers S on GRP.server = S.name
-order by server;
+    (SELECT 
+        server, SUM(number_pc) AS pc_number
+    FROM
+        kids K
+    WHERE
+        server != ''
+    GROUP BY server) GRP
+        LEFT JOIN
+    servers S ON GRP.server = S.name 
+UNION SELECT 
+    type, 
+    pc_number,
+    capacity - pc_number,
+    WEB.version
+FROM
+    (SELECT 
+        type, version, capacity
+    FROM
+        customer.servers
+    WHERE
+        type = 'WEB') WEB
+        LEFT JOIN
+    (SELECT 
+        SUM(number_pc) AS pc_number, version
+    FROM
+        kids K
+    RIGHT JOIN servers S ON S.name = K.server
+    GROUP BY version) GRP ON WEB.version = GRP.version;
