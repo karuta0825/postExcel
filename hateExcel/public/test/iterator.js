871b310156ffbdb95837f066d6081bd2c4ee4699
karuta0825 <magune3g@gmail.com>
2017-07-30 21:06:51 +0900

    Pageクラスを完成させる

--- hateExcel/public/test/iterator.js
+++ hateExcel/public/test/iterator.js
@@ -1,15 +1,92 @@
+
 var Page = function ( list, item_per_page ) {
+
+  if ( !list || !_.isArray(list) ) {
+    throw new Error('first arugument needs Array');
+  }
+
+  if ( !item_per_page ) {
+    throw new Error('two aruguments need.');
+  }
+
+  this['idx'] = 1;
+  this['list'] = list;
+  this['length'] = list.length;
+  this['visible_item_number'] = item_per_page;
+  this['max_page'] = Math.ceil( this.length / item_per_page );
+
+};
+
+Page.fn = Page.prototype;
+
+Page.fn.get = function ( page_number ) {
+
+  var
+    start =  this['visible_item_number'] * (page_number-1)
+  , end = start + this['visible_item_number']
+  ;
+
+  if ( page_number > this['max_page'] || page_number < 1 ) {
+    throw new Error('インデックスエラー');
+  }
+
+  this['idx'] = page_number;
+
+  return this['list'].slice( start, end );
+
+};
+
+Page.fn.next = function () {
+
+  if ( this['idx'] === this['max_page'] ) {
+    return null;
+  }
+
+  this['idx'] += 1;
+  return this.get(this['idx']);
+
+};
+
+Page.fn.prev = function () {
+
+  if ( this['idx'] === 1 ) {
+    return null;
+  }
+
+  this['idx'] -= 1;
+  return this.get(this['idx']);
+
+};
+
+Page.fn.current = function () {
+  return this.get(this['idx']);
+};
+
+Page.fn.getIndex = function () {
+  return this['idx'];
+};
+
+Page.fn.getMaxPage = function () {
+  return this['max_page'];
+};
+
+
+var Page = function ( list, item_per_page ) {
+
+  if ( !list || !_.isArray(list) ) {
+    throw new Error('first arugument needs Array');
+  }
 
   if ( !item_per_page ) {
     throw new Error('two aruguments need.');
   }
 
   var
-    idx    = 1
-  , data   = list
-  , length = list.length
-  , items  = item_per_page
-  , pages  = Math.ceil( length / item_per_page )
+    idx                  = 1
+  , data                 = list
+  , length               = list.length
+  , visible_item_number  = item_per_page
+  , max_page             = Math.ceil( length / item_per_page )
   // public
   , get
   , next
@@ -17,14 +94,19 @@ var Page = function ( list, item_per_page ) {
   , current
   ;
 
+  /**
+   * 指定したページ番号のアイテムを取得する
+   * @param  {Number} page_number - ページ番号
+   * @return {Array}
+   */
   get = function ( page_number ) {
 
     var
-      start =  items * (page_number-1)
-    , end = start + items
+      start =  visible_item_number * (page_number-1)
+    , end = start + visible_item_number
     ;
 
-    if ( page_number > pages || page_number < 1 ) {
+    if ( page_number > max_page || page_number < 1 ) {
       throw new Error('インデックスエラー');
     }
 
@@ -34,9 +116,14 @@ var Page = function ( list, item_per_page ) {
 
   };
 
+  /**
+   * 次の表示アイテムを取得
+   * 次が存在しない場合、nullを返す
+   * @return {Array}
+   */
   next = function () {
 
-    if ( idx === pages ) {
+    if ( idx === max_page ) {
       return null;
     }
 
@@ -45,9 +132,14 @@ var Page = function ( list, item_per_page ) {
 
   };
 
+  /**
+   * 前の表示アイテムを取得
+   * 前が存在しない場合、nullを返す
+   * @return {Array}
+   */
   prev = function () {
 
-    if ( idx === 0 ) {
+    if ( idx === 1 ) {
       return null;
     }
 
@@ -56,6 +148,10 @@ var Page = function ( list, item_per_page ) {
 
   };
 
+  /**
+   * 現在の表示ページのアイテムを取得
+   * @return {Array}
+   */
   current = function () {
     return get(idx);
   };
@@ -67,7 +163,7 @@ var Page = function ( list, item_per_page ) {
     prevPage    : prev,
     currentPage : current,
     getIndex    : function () { return idx; },
-    getMaxPage  : function () { return pages; }
+    getMaxPage  : function () { return max_page; }
   };
 
 };
@@ -75,4 +171,4 @@ var Page = function ( list, item_per_page ) {
 
 var list = [1,2,3,4,5,6,7,8,9,10];
 
-var i = Page(list, 3);
+  var i = Page(list, 3);
