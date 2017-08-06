9e554b0d09672f193a9cc4b2c08ec95731bba94e
karuta0825 <magune3g@gmail.com>
2017-08-06 22:03:17 +0900

    ある列が更新されたときのみトリガーが実行されるように対応

--- db/data/trigger_kids_update.sql
+++ db/data/trigger_kids_update.sql
@@ -5,19 +5,20 @@ DELIMITER $$
 
 CREATE TRIGGER kids_update AFTER update ON kids FOR EACH ROW
  BEGIN
-
-	if new.userkey != '' then
+ 
+	
+	if new.userkey != old.userkey then
 		update clients C 
 			set C.client_id = Concat( new.userkey, right(client_id,4) ) , 
 				C.client_pass = Concat( new.userkey, right(client_id,4) ) 
-			where kid = old.kid;
+			where kids_id = old.id;
     end if;
     
-	if new.fenics_key != '' then
+	if new.fenics_key != old.fenics_key then
 		update fenics F
 			set F.fenics_id = Concat( new.fenics_key, right(fenics_id,5) ), 
 				F.password = Concat( new.fenics_key, right(fenics_id,5) ) 
-			where kid = old.kid and is_mobile = 0;
+			where kids_id = old.id and is_mobile = 0;
     end if;
 
  END;
