4c26f1d3a880cae55224e270b94f03d9d2b3a5ed
karuta0825 <magune3g@gmail.com>
2017-08-19 13:25:54 +0900

    メモテンプレートモデルの追加

---
+++ hateExcel/public/js/model/model.memo.template.js
@@ -0,0 +1,112 @@
+
+(function ( $, cms ) {
+
+  var
+    config = {'table' : 'memo_templates'}
+  , _model = new Model(config)
+  , _selected
+  , _vl = util.Validate({
+      'id'    : 'noCheck',
+      'title' : 'isEmpty',
+      'msg'   : 'isEmpty'
+    })
+  , find
+  , insert
+  , update
+  , remove
+  ;
+
+  setSelectedItem = function ( id ) {
+    _selected = id;
+  };
+
+  getSelectedItem = function () {
+    return _selected;
+  };
+
+  find = function ( condition, callback ) {
+
+    _model.find( condition, callback);
+
+    setSelectedItem(condition.id);
+
+  };
+
+  insert = function ( data, cb_success, cb_fail ) {
+
+    var errs = _vl.validate(data);
+
+    if ( errs && errs.length > 0 ) {
+      cb_fail(errs);
+      return;
+    };
+
+    cms.db.post('/insert', {
+      'data'  : [data],
+      'table' : 'memo_templates'
+    })
+    .then( function () {
+      cb_success();
+    })
+    ;
+
+
+  };
+
+  update = function ( data, cb_success, cb_fail ) {
+
+    var
+      id   = getSelectedItem()
+    , errs = _vl.validate(data)
+    , params
+    ;
+
+    if ( errs && errs.length > 0 ) {
+      cb_fail(errs);
+      return;
+    };
+
+    params = {
+      data      : data,
+      condition : { id : id },
+      table     : 'memo_templates'
+    };
+
+    cms.db.post('/update', params )
+    .then( function () {
+      cb_success();
+    })
+    ;
+
+  };
+
+  remove = function ( callback ) {
+
+    // 対象を見つける
+    var id = getSelectedItem();
+
+    params = {
+      data  : [{ id : id }],
+      table : 'memo_templates'
+    };
+
+    cms.db.post('/delete', params )
+    .then( function () {
+      callback();
+    })
+    ;
+
+  };
+
+  cms.model.memoTemplate = {
+    fetch           : $.proxy( _model.fetchAsync, _model ),
+    getCache        : $.proxy( _model.getCache, _model ),
+    find            : find,
+    insert          : insert,
+    update          : update,
+    remove          : remove,
+    setSelectedItem : setSelectedItem,
+    getSelectedItem : getSelectedItem
+  };
+
+}( jQuery, customer ))
