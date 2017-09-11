ef9e54b510839e4c8fecd6c393dc205ec6034e47
karuta0825 <magune3g@gmail.com>
2017-09-11 21:56:03 +0900

    テストコード追加

--- hateExcel/test/unit/test_model.kids.js
+++ hateExcel/test/unit/test_model.kids.js
@@ -8,13 +8,13 @@ import DATA from '../fixtures/data/kids';
 describe('model.kidsモジュール', () => {
 
 
-  describe('initModule & getHeader', () => {
+  describe('initModule & getHeaderメソッド', () => {
 
     it('サーバーより取得したヘッダー情報を返す');
 
   });
 
-  describe('fetch & getCache', () => {
+  describe('fetch & getCacheメソッド', () => {
 
     it('サーバーより取得した全ユーザーデータを返す', () => {
 
@@ -36,7 +36,7 @@ describe('model.kidsモジュール', () => {
 
   });
 
-  describe('getData', () => {
+  describe('getDataメソッド', () => {
 
     it('メモリキャッシュに保存した全ユーザーデータでページオブジェクトを初期化して、現在のページを返す', () => {
 
@@ -80,7 +80,7 @@ describe('model.kidsモジュール', () => {
   });
 
 
-  describe('find', () => {
+  describe('findメソッド', () => {
 
     it('id=1で検索した結果を取得', () => {
 
@@ -138,7 +138,7 @@ describe('model.kidsモジュール', () => {
 
   });
 
-  describe('delete', () => {
+  describe('deleteメソッド', () => {
 
     it('id=1のユーザーデータを削除する');
 
@@ -146,27 +146,210 @@ describe('model.kidsモジュール', () => {
 
   describe('getFilter', () => {
 
-    it('初期状態のフィルター結果を返す');
+    var fs, spy;
 
-    it('システム情報"cloud"でフィルター後、フィルター結果を返す');
+    // 前処理
+    beforeEach(() => {
+      fs = new FakeServer();
+      spy = sinon.spy();
+      fs.setFetch();
+    });
+
+    // 後処理
+    afterEach( () => {
+      customer.model.kids.setCondition({system:'all'});
+      fs.destory();
+    });
+
+    it('fetch直後は、全データを返す', () => {
+
+      return customer.model.kids.fetch(null,spy)
+      .then( () => {
+        assert( customer.model.kids.getFilter().length === DATA.fetch.list.out.length );
+        assert.deepEqual( customer.model.kids.getFilter(), DATA.fetch.list.out );
+      });
+
+    });
+
+    it('システム情報"cloud"でフィルター後、フィルター結果を返す', () => {
+
+      return customer.model.kids.fetch( null, spy )
+      .then( () => {
+
+        customer.model.kids.setCondition({system:'cloud'});
+
+        assert( customer.model.kids.getFilter().length === 23 );
+
+      });
+
+
+    });
 
   });
 
-  describe('getCondition', () => {
+  describe('getConditionメソッド', () => {
 
-    it('初期状態では全プロパティ=allで返す');
+    var fs, spy;
+
+    beforeEach( () => {
+      fs = new FakeServer();
+      spy = sinon.spy();
+      fs.setFetch();
+    });
+
+    afterEach( () => {
+      customer.model.kids.setCondition({system:'all'});
+      fs.destory();
+    });
+
+    it('fetch直後は全データでページオブジェクトを初期化して１ページ目を返す', () => {
+
+      return customer.model.kids.fetch( null, spy )
+      .then( () => {
+
+        assert( customer.model.kids.getCondition().length === 30 );
+        assert.deepEqual( customer.model.kids.getCondition(), DATA.fetch.list.out.slice( 0, 30 ));
+
+      });
+
+    });
+
+    it('システム情報"cloud"でフィルター後、フィルタ結果でページオブジェクトを初期化して１ページ目を返す', () => {
+
+      return customer.model.kids.fetch( null, spy )
+      .then( () => {
+
+        customer.model.kids.setCondition({system:'cloud'});
+
+        assert( customer.model.kids.getCondition().length === 23 );
+
+      });
+
+    });
+
+    it('引数にコールバックを指定した場合、1ページ目を引数にコールバック関数を実行する', () => {
+
+      var spy2 = sinon.spy();
+
+      return customer.model.kids.fetch( null, spy )
+      .then( () => {
+
+        customer.model.kids.getCondition( spy2 );
+
+        assert( spy2.called === true );
+
+      });
+
+    });
 
   });
 
-  describe('setCondition', () => {
+  describe('getConditionAllメソッド', () => {
+
+    var fs, spy;
+
+    before( () => {
+      fs = new FakeServer();
+      spy = sinon.spy();
+      fs.setFetch();
+    });
+
+    after( () => {
+      customer.model.kids.setCondition({system:"all"});
+      fs.destory();
+    });
+
+    it('システム情報"onpre"でフィルター後、その結果を返す', () => {
+
+      return customer.model.kids.fetch( null, spy )
+      .then( () => {
+
+        customer.model.kids.setCondition({system:"onpre"});
+
+        assert( customer.model.kids.getConditionAll().length === 10 );
+
+      });
+
+    });
 
   });
+
+  describe('setConditionメソッド', () => {
+
+    var fs, spy;
+
+    beforeEach( () => {
+      fs = new FakeServer();
+      spy = sinon.spy();
+      fs.setFetch();
+    });
+
+    afterEach( () => {
+      // フィルター条件を元に戻す
+      customer.model.kids.setCondition({system:'all'});
+      fs.destory();
+    });
+
+    it('引数にコールバックを指定した場合、1ページ目を引数にコールバック関数を実行する', () => {
+
+      var spy2 = sinon.spy();
+
+      return customer.model.kids.fetch( null, spy )
+      .then( () => {
+
+        customer.model.kids.setCondition({system:"cloud"}, spy2 );
+
+        assert( spy2.called === true );
+
+      });
+
+    });
+
+
+  });
+
 
   describe('sortByCol', () => {
 
-    it('全ユーザー情報を"kid"で降順に並び替えた結果を返す');
+    var fs, spy;
 
-    it('全ユーザー情報を"kid"で昇順に並び替えた結果を返す');
+    beforeEach( () => {
+      fs = new FakeServer();
+      spy = sinon.spy();
+      fs.setFetch();
+    });
+
+    afterEach( () => {
+      fs.destory();
+    });
+
+    it('全ユーザー情報を"kid"で降順に並び替えた結果を返す', () => {
+
+      return customer.model.kids.fetch( null, spy )
+      .then( () => {
+
+        customer.model.kids.sortByCol('id');
+        customer.model.kids.sortByCol('id');
+
+        assert( customer.model.kids.getCache()[0].id === 99 );
+
+      });
+
+    });
+
+    it('全ユーザー情報を"kid"で昇順に並び替えた結果を返す', () => {
+
+      return customer.model.kids.fetch( null, spy )
+      .then( () => {
+
+        customer.model.kids.sortByCol('id');
+
+        assert( customer.model.kids.getCache()[0].id === 1 );
+
+      });
+
+    });
+
 
   });
 
@@ -185,35 +368,33 @@ describe('model.kidsモジュール', () => {
 
   describe('search', () => {
 
-    it('KID55をキーワードに検索すると、KID55を含む検索結果一覧を返す', () => {
+    var fs, spy;
 
-      var fs = new FakeServer();
-      var spy = sinon.spy();
-
+    beforeEach( () => {
+      fs = new FakeServer();
+      spy = sinon.spy();
       fs.setFetch();
+    });
+
+    afterEach( () => {
+      fs.destory();
+    });
+
+    it('KID55をキーワードに検索すると、KID55を含む検索結果一覧を返す', () => {
 
       return customer.model.kids.fetch( null, spy )
       .then( () => {
-        fs.destory();
-      })
-      .then( () => {
+        assert( customer.model.kids.search('KID55').length === 30);
         assert.deepEqual( customer.model.kids.search('KID55'),  DATA.fetch.list.out.slice( 0, 30 )) ;
-      })
-      ;
+      });
 
     });
 
     it('KID55をキーワードに検索して、コールバックをしているすると検索結果を引数にしてコールバックを実行する', () => {
-      var fs = new FakeServer();
-      var spy = sinon.spy();
+
       var spy2 = sinon.spy();
 
-      fs.setFetch();
-
       return customer.model.kids.fetch( null, spy )
-      .then( () => {
-        fs.destory();
-      })
       .then( () => {
         customer.model.kids.search('KID55', spy2 );
         assert( spy2.called === true );
@@ -227,17 +408,21 @@ describe('model.kidsモジュール', () => {
 
   describe('getPageList', () => {
 
+    var fs, spy;
+
+    beforeEach( () => {
+      fs = new FakeServer();
+      spy = sinon.spy();
+      fs.setFetch();
+    });
+
+    afterEach( () => {
+      fs.destory();
+    });
+
     it('ページ情報を取得 全データ33件よりページは2ページ存在', () => {
 
-      var fs = new FakeServer();
-      var spy = sinon.spy();
-
-      fs.setFetch();
-
       return customer.model.kids.fetch( null, spy )
-      .then( () => {
-        fs.destory();
-      })
       .then( () => {
         assert.deepEqual( customer.model.kids.getPageList(), [1,2] );
       })
@@ -247,16 +432,9 @@ describe('model.kidsモジュール', () => {
 
     it('引数にコールバックを指定した場合、ページ情報を引数にコールバック関数を実行する', () => {
 
-      var fs = new FakeServer();
-      var spy = sinon.spy();
       var spy2 = sinon.spy();
 
-      fs.setFetch();
-
       return customer.model.kids.fetch( null, spy )
-      .then( () => {
-        fs.destory();
-      })
       .then( () => {
         customer.model.kids.getPageList(spy2);
         // 呼ばれたかどうか
