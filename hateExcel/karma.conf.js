d32e2ebed5c0ef5a9de3ef1a90dc0626e44d6b63
karuta0825 <karuta0825@users.noreply.github.com>
2017-09-11 19:07:31 +0900

    karmaロードファイルにクライアント全ファイルを適用

--- hateExcel/karma.conf.js
+++ hateExcel/karma.conf.js
@@ -22,10 +22,51 @@ module.exports = function(config) {
       "public/js/class/Model.js",
       "public/js/class/Page.js",
       "public/js/model/model.home.serverTable.js",
-      "public/js/model/model.user.memos.js",
+      "public/js/model/model.home.graph.js",
+      "public/js/model/model.home.events.js",
+      "public/js/model/model.kids.js",
+      "public/js/model/model.fenics.js",
+      "public/js/model/model.servers.js",
+      "public/js/model/model.services.js",
+      "public/js/model/model.loginUser.js",
+      "public/js/model/model.loginUsers.js",
+      "public/js/model/model.memo.template.js",
+      "public/js/model/model.historys.js",
+      "public/js/model/model.environments.js",
       "public/js/model/model.user.base.js",
-      "public/js/view/view.edit.user.js",
+      "public/js/model/model.user.customer.js",
+      "public/js/model/model.user.license.js",
+      "public/js/model/model.user.clients.js",
+      "public/js/model/model.user.partner.js",
+      "public/js/model/model.user.history.js",
+      "public/js/model/model.user.network.js",
+      "public/js/model/model.user.busiv.js",
+      "public/js/model/model.user.mobile.js",
+      "public/js/model/model.user.memos.js",
+      "public/js/view/view.home.js",
+      "public/js/view/view.home.serverTable.js",
+      "public/js/view/view.home.graph.js",
+      "public/js/view/view.home.calendar.js",
+      "public/js/view/view.make.user.js",
+      "public/js/view/view.register.user.js",
       "public/js/view/view.kids.js",
+      "public/js/view/view.fenics.js",
+      "public/js/view/view.edit.user.js",
+      "public/js/view/view.user.base.js",
+      "public/js/view/view.user.service.js",
+      "public/js/view/view.user.client.js",
+      "public/js/view/view.user.partner.js",
+      "public/js/view/view.user.network.js",
+      "public/js/view/view.user.fenics.js",
+      "public/js/view/view.user.busiv.js",
+      "public/js/view/view.user.mobile.js",
+      "public/js/view/view.user.history.js",
+      "public/js/view/view.user.memo.js",
+      "public/js/view/view.setting.servers.js",
+      "public/js/view/view.setting.services.js",
+      "public/js/view/view.setting.memo.template.js",
+      "public/js/view/view.setting.login.js",
+      "public/js/view/view.dialog.fenics.js",
       "views/index.ejs",
       "public/html/*.html",
       "public/template/*.html",
@@ -42,16 +83,16 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      "views/index.ejs" : "html2js",
-      "public/html/*.html" : "html2js",
-      "public/template/*.html" : "html2js",
-      "public/js/class/*.js" : ['coverage'],
-      "public/js/model/*.js" : ['coverage'],
-      "public/js/view/*.js" : ['coverage'],
-      "public/js/*.js" : ['coverage'],
-      "test/fixtures/*.html": "html2js",
-      "test/fixtures/server/*.js": ['webpack', 'sourcemap'],
-      'test/unit/*.js': ['webpack','sourcemap']
+      "views/index.ejs"           : "html2js",
+      "public/html/*.html"        : "html2js",
+      "public/template/*.html"    : "html2js",
+      "public/js/class/*.js"      : ['coverage'],
+      "public/js/model/*.js"      : ['coverage'],
+      "public/js/view/*.js"       : ['coverage'],
+      "public/js/*.js"            : ['coverage'],
+      "test/fixtures/*.html"      : "html2js",
+      "test/fixtures/server/*.js" : ['webpack', 'sourcemap'],
+      'test/unit/*.js'            : ['webpack','sourcemap']
     },
 
     webpack: {
@@ -75,7 +116,7 @@ module.exports = function(config) {
         postLoaders: [
           {
             test: /\.js$/,
-            exclude: /(test|node_modules)\//,
+            exclude: /(test|node_modules)/,
             loader: 'istanbul-instrumenter'
           }
         ]
@@ -128,5 +169,5 @@ module.exports = function(config) {
       ]
     }
 
-  })
-}
+  });
+};
