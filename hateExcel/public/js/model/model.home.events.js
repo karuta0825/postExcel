21c9e3ff9f843e4e9e093b09518885a09742a0f9
karuta0825 <karuta0825@users.noreply.github.com>
2017-09-12 20:00:48 +0900

    通信をPromsiceに変更

--- hateExcel/public/js/model/model.home.events.js
+++ hateExcel/public/js/model/model.home.events.js
@@ -14,7 +14,7 @@
       'start_on'   : 'isEmpty',
       'start_time' : 'noCheck',
       'message'    : 'noCheck',
-      'is_finished' : 'noCheck'
+      'is_finished': 'noCheck'
     })
   , validate
   , insert
@@ -22,6 +22,17 @@
   , remove
   ;
 
+  /**
+   * 新規イベントの作成
+   * @param  {String} month           - YYYY-MM
+   * @param  {Object} data            - 作成イベントオブジェクト
+   * @param  {String} data.title      - タイトル
+   * @param  {String} data.start_on   - 開始日付
+   * @param  {String} data.start_time - 開始時間
+   * @param  {String} data.message    - イベント内容
+   * @param  {Function} cb_success    - イベント作成完了時コールバック
+   * @param  {Function} cb_fail       - イベント作成失敗時コールバック
+   */
   insert = function ( month, data, cb_success, cb_fail ) {
 
     var
@@ -37,13 +48,27 @@
       return;
     }
 
-    cms.db.post('/insert', params )
+    return cms.db.post('/insert', params )
     .then( function () {
-      _model.fetch( month , cb_success );
+      return _model.fetchAsync( month );
+    })
+    .then( function (r) {
+      cb_success(r);
     });
 
   };
 
+  /**
+   * イベントの更新
+   * @param  {String} month           - YYYY-MM
+   * @param  {Object} data            - 作成イベントオブジェクト
+   * @param  {String} data.title      - タイトル
+   * @param  {String} data.start_on   - 開始日付
+   * @param  {String} data.start_time - 開始時間
+   * @param  {String} data.message    - イベント内容
+   * @param  {Function} cb_success    - イベント更新完了時コールバック
+   * @param  {Function} cb_fail       - イベント更新失敗時コールバック
+   */
   update = function ( month, data, cb_success, cb_fail ) {
 
     var
@@ -67,13 +92,23 @@
       table     : 'events'
     };
 
-    cms.db.update('/update', param, function () {
-      _model.fetch( month, cb_success );
+    return cms.db.post('/update', param )
+    .then( function () {
+      return _model.fetchAsync( month );
+    })
+    .then( function (r) {
+      cb_success(r);
     });
 
 
   };
 
+  /**
+   * イベントの削除
+   * @param  {String} month      - YYYY-MM
+   * @param  {String} id         - イベントID
+   * @param  {Function} callback - イベント削除時コールバック
+   */
   remove = function ( month, id, callback ) {
 
     var param = {
@@ -81,8 +116,12 @@
       table : 'events'
     };
 
-    cms.db.remove('/delete', param, function () {
-      _model.fetch( month, callback );
+    return cms.db.post('/delete', param )
+    .then( function () {
+      return _model.fetchAsync( month );
+    })
+    .then( function (r) {
+      callback(r);
     });
 
   };
@@ -91,7 +130,7 @@
 
   // to public
   cms.model.homeEvents = {
-    fetch    : $.proxy( _model.fetch, _model ),
+    fetch    : $.proxy( _model.fetchAsync, _model ),
     getCache : $.proxy( _model.getCache, _model ),
     find     : $.proxy( _model.find, _model ),
     insert   : insert,
