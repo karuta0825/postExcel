7c2ca8a5d8c60c981ce8d2e31f65221689a49ecc
karuta0825 <magune3g@gmail.com>
2017-11-11 18:53:43 +0900

    トランザクション処理をdatabaseクラスに追加

--- hateExcel/models/database.js
+++ hateExcel/models/database.js
@@ -23,7 +23,7 @@ Database.prototype._getClient = function () {
 Database.prototype.query = function (query, params, callback) {
   var client = this._getClient();
   return client.query(query, params, callback);
-}
+};
 
 // クエリを終了する
 Database.prototype.end = function (callback) {
@@ -31,7 +31,65 @@ Database.prototype.end = function (callback) {
     this.client.end(callback);
     delete this.client;
   }
+};
+
+Database.prototype.transaction = function ( querys, params, callback ) {
+  var client = this._getClient();
+  var size = querys.length;
+
+  return new Promise( function( resolve, reject ) {
+
+    // 再起処理関数の作成
+    function loop (i) {
+
+      return new Promise( function (res, rej) {
+        // 非同期処理
+        client.query( querys[i], params[i], function (err, result) {
+          if (err) { rej(err); }
+          res(i+1);
+        });
+      })
+      .then( function (count) {
+
+        if ( count < size ) {
+          // 次の処理
+          loop(count);
+        } else {
+          // 終了
+          client.commit( function (err) {
+            client.end();
+            if (err) {reject(err);}
+            resolve('ends');
+          });
+        }
+
+      })
+      .catch( function (err) {
+        client.rollback( function () {
+          reject(err)
+        });
+      });
+
+    }
+
+    // メインループ処理
+    client.beginTransaction( function (err) {
+      if( err ) { reject(err) };
+      loop(0);
+    });
+
+  })
+  .then( function (r) {
+    callback(null, r);
+  })
+  .catch( function (err) {
+    callback(err, null);
+    client.end();
+  })
+  ;
+
 }
+
 
 // Databaseクラスのインスタンスを作成する
 function createClient() {
