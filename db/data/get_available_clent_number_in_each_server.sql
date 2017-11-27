198819221492bd5e62fab4d854813320f5c2f381
karuta0825 <magune3g@gmail.com>
2017-11-27 20:22:16 +0900

    クライアント数ごとの取得するようにSQL修正

---
+++ db/data/get_available_clent_number_in_each_server.sql
@@ -0,0 +1,66 @@
+
+-- APのみ
+SELECT 
+    S.name,
+    IFNULL(client_number, 0) AS client_number,
+    capacity - IFNULL(client_number, 0) AS available_number,
+    version
+FROM
+    servers S
+        LEFT JOIN
+    (SELECT 
+        server, SUM(client_number) AS client_number
+    FROM
+        kids K
+    LEFT JOIN (SELECT 
+        kids_id, COUNT(kids_id) AS client_number
+    FROM
+        clients C
+    WHERE
+        is_admin = 0 AND end_on IS NULL
+    GROUP BY kids_id) SUM_CLI ON K.id = SUM_CLI.kids_id
+    GROUP BY server) SRVS ON S.name = SRVS.server
+WHERE
+    type = 'AP'
+union all 
+-- DBのみ
+SELECT 
+    S.name, 
+    IFNULL(client_number, 0) AS client_number,
+    S.capacity - IFNULL(client_number, 0) as available_number,
+    version
+FROM
+    servers S
+RIGHT JOIN
+    (
+		-- 各APサーバーの接続先のDBごとのクライアント数をもとめる
+		SELECT 
+			connect_db AS name,
+			SUM(IFNULL(client_number, 0)) AS client_number
+		FROM
+			servers S
+		LEFT JOIN 
+        -- APサーバごとの接続クライアント数をもとめる
+		(
+			SELECT 
+				server, SUM(client_number) AS client_number
+			FROM
+				kids K
+			LEFT JOIN 
+			(
+				SELECT 
+					kids_id, COUNT(kids_id) AS client_number
+				FROM
+					clients C
+				WHERE
+					is_admin = 0 AND end_on IS NULL
+				GROUP BY kids_id
+			) SUM_CLI 
+			ON K.id = SUM_CLI.kids_id
+			GROUP BY server
+		) SRVS ON S.name = SRVS.server where server is not null
+		GROUP BY connect_db 
+    ) SUM_DB_SRVS
+ON S.name = SUM_DB_SRVS.name
+order by name
+
