f287bb25c1cf3bf87884a4a5edbc5935d2d44acf
karuta0825 <magune3g@gmail.com>
2017-11-28 08:02:37 +0900

    webサーバをリストに追加

--- db/data/get_available_clent_number_in_each_server.sql
+++ db/data/get_available_clent_number_in_each_server.sql
@@ -62,5 +62,38 @@ RIGHT JOIN
 		GROUP BY connect_db 
     ) SUM_DB_SRVS
 ON S.name = SUM_DB_SRVS.name
+union all
+SELECT 
+    name,
+    client_number, 
+    S.capacity - client_number as available_number,
+    S.version
+FROM
+    (SELECT 
+        version, SUM(client_number) AS client_number
+    FROM
+        (SELECT 
+        S.name, IFNULL(client_number, 0) AS client_number, version
+    FROM
+        servers S
+    LEFT JOIN (SELECT 
+        server, SUM(client_number) AS client_number
+    FROM
+        kids K
+    LEFT JOIN (SELECT 
+        kids_id, COUNT(kids_id) AS client_number
+    FROM
+        clients C
+    WHERE
+        is_admin = 0 AND end_on IS NULL
+    GROUP BY kids_id) SUM_CLI ON K.id = SUM_CLI.kids_id
+    GROUP BY server) SUM_SRVS_AP ON SUM_SRVS_AP.server = S.name
+    WHERE
+        type = 'AP') SUM_WEB
+    GROUP BY version) ADD_NAME
+        LEFT JOIN
+    servers S ON ADD_NAME.version = S.version
+WHERE
+    type = 'WEB'
 order by name
 
