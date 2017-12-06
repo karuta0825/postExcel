1779b7d0682be6fa81c7efae8175f0cf3bfce79c
karuta0825 <magune3g@gmail.com>
2017-12-07 08:16:47 +0900

    ユーザ登録のファイル分割を行う

---
+++ hateExcel/public/js/model/model.register.user.js
@@ -0,0 +1,209 @@
+
+
+(function ($, cms) {
+
+  var
+     uploadData
+  , _makeSetUpInfo
+  , getUploadData
+  , getLineList
+  , makeUploadData
+  , upload
+  ;
+
+  _makeSetUpInfo = function ( item, map, value ) {
+
+    value =  ( value === '' ) ? 0 : Number( value.trim() );
+
+    // ライセンス情報のとき
+    if ( item['is_setup_info'] !== 1  ) {
+      map['licenses'][item.service_id] = value;
+    }
+    // セットアップ情報のとき
+    else {
+
+      // ネットワーク
+      if ( item.service_id === 'has_busiv' ) {
+        map['customers']['has_busiv'] = ( value === 1 ) ? 1 : 0;
+        map['customers']['has_fenics'] = ( value === 1 ) ? 0 : 1;
+      }
+
+      // モバイル使用
+      if ( item.service_id === 'has_sd' ) {
+        map['licenses']['K1'] = 1;
+        map['customers']['has_mobile'] = value;
+      }
+
+      // 追加1クライアント数
+      if ( item.service_id === 'add_one_cli' ) {
+        map['kids']['number_pc'] += Number(value);
+      }
+
+      // 追加10クライアント数
+      if ( item.service_id === 'add_ten_cli' ) {
+        map['kids']['number_pc'] += 10 * Number(value);
+      }
+
+      // 追加20クライアント数
+      if ( item.service_id === 'add_twe_cli' ) {
+        map['kids']['number_pc'] += 20 * Number(value);
+      }
+
+      // 追加ユーザー
+      if ( item.service_id === 'add_one_usr' ) {
+        map['clients']['number'] += value;
+      }
+
+      if ( item.service_id === 'add_sd' ) {
+        map['mobiles']['number'] = value;
+      }
+
+    }
+
+  };
+
+  /**
+  * @param  {File}          - fileReader
+  * @return {Array<String>} - list_oneline
+  */
+  getLineList = function ( fileReader ) {
+    var
+      line_code = fileReader.result.indexOf('\r') === -1 ? '\n' : '\r\n'
+    , list_oneline = fileReader.result.split(line_code)
+    ;
+
+    list_oneline = _.select( list_oneline, function (val, key) {
+      if ( val !== '' && val.indexOf('#') !== 0 ) {
+        return val;
+      }
+    });
+
+    return list_oneline;
+  };
+
+  makeUploadData = function ( list_oneline, kid ) {
+
+    var
+      id = cms.model.kids.find({'kid' : kid})[0].id
+    , version = cms.model.kids.find({'kid':kid})[0].version
+    , map_result = {
+        'kids'      : { 'id' : id, 'number_pc' : 1 },
+        'clients'   : { 'kids_id' : id, 'number' : 1 },
+        'customers' : { 'kids_id' : id },
+        'licenses'  : { 'kids_id' : id },
+        'partners'  : { 'kids_id' : id },
+        'mobiles'   : { 'kids_id' : id, 'number' : 1 },
+        'busivs'    : { 'kids_id' : id }
+      }
+    , delimiter_position
+    , key
+    , val
+    , table
+    , field
+    ;
+
+    // データ作成
+    _.each( list_oneline, function (v,k) {
+
+      delimiter_position = v.indexOf(':');
+      key = v.slice(0,delimiter_position);
+      val = v.slice(delimiter_position+1);
+
+      table = key.split('__')[0];
+      field = key.split('__')[1].trim();
+
+      if ( !map_result.hasOwnProperty(table) ){
+        throw Error( '存在しないテーブル(' + table + ')があるため失敗しました' );
+      }
+
+      if ( table === 'licenses') {
+        var item = cms.model.services.find({ 'version' : version, 'sales_id' : field })[0];
+        _makeSetUpInfo(item, map_result, val );
+      }
+      else {
+        map_result[table][field] = val.trim();
+      }
+
+    });
+
+    uploadData =  map_result;
+    uploadData.kid = kid;
+
+    console.log(uploadData);
+  }
+
+  upload = function () {
+
+    // kids -端末数
+    cms.model.kids.register({
+      kid                      : uploadData.kid,
+      user_name                : uploadData['kids']['user_name'],
+      kana                     : uploadData['kids']['kana'],
+      number_pc                : uploadData['kids']['number_pc'],
+      has_qa                   : ( uploadData['kids']['has_qa']                   === '1') ? '1' : '0',
+      is_new_contract          : ( uploadData['kids']['is_new_contract']          === '1') ? '1' : '0',
+      is_replaced_from_cj      : ( uploadData['kids']['is_replaced_from_cj']      === '1') ? '1' : '0',
+      is_replaced_from_wc      : ( uploadData['kids']['is_replaced_from_wc']      === '1') ? '1' : '0',
+      is_replaced_from_another : ( uploadData['kids']['is_replaced_from_another'] === '1') ? '1' : '0',
+      register_on              : moment().format('YYYY-MM-DD'),
+      is_registered            : 1
+    })
+    // customers
+    .then( function () {
+      return cms.model.userCustomer.register( uploadData.customers );
+    })
+    // licenses
+    .then( function () {
+      return customer.model.userLicense.register( uploadData.licenses );
+    })
+    // partners
+    .then( function () {
+      return cms.model.userPartner.register( uploadData.partners );
+    })
+    // クライアント - ユーザー数
+    .then( function () {
+      // kid , userkey が必要
+      return cms.model.userBaseInfo.registerClient({
+        kids_id             : uploadData['kids']['id'],
+        userkey             : cms.model.kids.find({'kid' : uploadData.kid})[0].userkey,
+        number_client_added : uploadData['clients']['number']
+      });
+
+    })
+    // ネットワーク - クライアント数
+    .then( function () {
+      if ( uploadData['customers']['has_fenics'] === 1 ) {
+        return cms.model.userNetwork.registerFenicsAccount({
+          kids_id         : uploadData['kids']['id'],
+          fenics_key      : cms.model.kids.find({'kid' : uploadData.kid})[0].fenics_key,
+          number_pc_added : uploadData['kids']['number_pc']
+        });
+      }
+    })
+    // mobiles
+    .then( function () {
+      return cms.model.userMobile.register( uploadData.mobiles );
+    })
+    .then( function () {
+      return cms.model.userBusiv.register( uploadData.busivs );
+    })
+    // all refresh
+    .then( function () {
+      cms.view.kids.refresh();
+      cms.view.regUsrs.showSuccess();
+    })
+    .catch( function (err) {
+      cms.view.regUsrs.showError(err.message);
+    })
+    ;
+
+
+  };
+
+  cms.model.regUsrs = {
+    getLineList    : getLineList,
+    makeUploadData : makeUploadData,
+    upload : upload
+  };
+
+}( jQuery, customer ));
--- hateExcel/public/js/view/view.register.user.js
+++ hateExcel/public/js/view/view.register.user.js
@@ -16,14 +16,9 @@
       'alert'  : '#modal-alert-register',
       'finish' : '#modal-finish-register'
     }
-  , uploadData
   , _onClickUpload
   , _selectFile
   , _dragOver
-  , _upload
-  , _makeUploadData
-  , _makeSetUpInfo
-  , _getLineList
   , _alertDialog
   , initModule
   ;
@@ -43,26 +38,9 @@
 
   _onClickUpload = function () {
     // upload処理
-    _upload( uploadData );
+    cms.model.regUsrs.upload();
     // ボタン非活性
     registerView.get('btn__upload').prop('disabled', true);
-
-  };
-
-  _getLineList = function ( fileReader ) {
-
-    var
-      line_code = fileReader.result.indexOf('\r') === -1 ? '\n' : '\r\n'
-    , list_oneline = fileReader.result.split(line_code)
-    ;
-
-    list_oneline = _.select( list_oneline, function (val, key) {
-      if ( val !== '' && val.indexOf('#') !== 0 ) {
-        return val;
-      }
-    });
-
-    return list_oneline;
 
   };
 
@@ -81,7 +59,7 @@
     r.onload = function ( e ) {
 
       // １行分の情報を取得
-      list_oneline = _getLineList(r);
+      list_oneline = cms.model.regUsrs.getLineList(r);
 
       // 1行目はKID
       kid = list_oneline.shift();
@@ -100,14 +78,11 @@
 
       // データ作成
       try {
-        uploadData = _makeUploadData( list_oneline, kid );
+        cms.model.regUsrs.makeUploadData( list_oneline, kid );
       } catch (e) {
         _alertDialog( e.msg );
         return;
       }
-
-      // check
-      console.log(uploadData);
 
       // ボタン状態制御
       registerView.get('btn__upload').prop('disabled', false);
@@ -116,177 +91,10 @@
 
   };
 
-  _makeUploadData = function ( list_oneline, kid ) {
-
-      var
-        id = cms.model.kids.find({'kid' : kid})[0].id
-      , version = cms.model.kids.find({'kid':kid})[0].version
-      , map_result = {
-          'kids'      : { 'id' : id, 'number_pc' : 1 },
-          'clients'   : { 'kids_id' : id, 'number' : 1 },
-          'customers' : { 'kids_id' : id },
-          'licenses'  : { 'kids_id' : id },
-          'partners'  : { 'kids_id' : id },
-          'mobiles'   : { 'kids_id' : id, 'number' : 1 },
-          'busivs'    : { 'kids_id' : id }
-        }
-      , delimiter_position
-      , key
-      , val
-      , table
-      , field
-      ;
-
-    // データ作成
-    _.each( list_oneline, function (v,k) {
-
-      delimiter_position = v.indexOf(':');
-      key = v.slice(0,delimiter_position);
-      val = v.slice(delimiter_position+1);
-
-      table = key.split('__')[0];
-      field = key.split('__')[1].trim();
-
-      if ( !map_result.hasOwnProperty(table) ){
-        throw Error( '存在しないテーブル(' + table + ')があるため失敗しました' );
-      }
-
-      if ( table === 'licenses') {
-        var item = cms.model.services.find({ 'version' : version, 'sales_id' : field })[0];
-        _makeSetUpInfo(item, map_result, val );
-      }
-      else {
-        map_result[table][field] = val.trim();
-      }
-
-    });
-
-    return map_result;
-
-  };
-
-  _makeSetUpInfo = function ( item, map, value ) {
-
-    value =  ( value === '' ) ? 0 : Number( value.trim() );
-
-    // ライセンス情報のとき
-    if ( item['is_setup_info'] !== 1  ) {
-      map['licenses'][item.service_id] = value;
-    }
-    // セットアップ情報のとき
-    else {
-
-      // ネットワーク
-      if ( item.service_id === 'has_busiv' ) {
-        map['customers']['has_busiv'] = ( value === 1 ) ? 1 : 0;
-        map['customers']['has_fenics'] = ( value === 1 ) ? 0 : 1;
-      }
-
-      // モバイル使用
-      if ( item.service_id === 'has_sd' ) {
-        map['licenses']['K1'] = 1;
-        map['customers']['has_mobile'] = value;
-      }
-
-      // 追加1クライアント数
-      if ( item.service_id === 'add_one_cli' ) {
-        map['kids']['number_pc'] += Number(value);
-      }
-
-      // 追加10クライアント数
-      if ( item.service_id === 'add_ten_cli' ) {
-        map['kids']['number_pc'] += 10 * Number(value);
-      }
-
-      // 追加20クライアント数
-      if ( item.service_id === 'add_twe_cli' ) {
-        map['kids']['number_pc'] += 20 * Number(value);
-      }
-
-      // 追加ユーザー
-      if ( item.service_id === 'add_one_usr' ) {
-        map['clients']['number'] += value;
-      }
-
-      if ( item.service_id === 'add_sd' ) {
-        map['mobiles']['number'] = value;
-      }
-
-    }
-
-  };
-
   _dragOver = function ( evt ) {
     evt.stopPropagation();
     evt.preventDefault();
     evt.dataTransfer.dropEffect = 'copy';
-  };
-
-  _upload = function ( upload_data ) {
-
-    // kids -端末数
-    cms.model.kids.register({
-      kid                      : kid,
-      user_name                : upload_data['kids']['user_name'],
-      kana                     : upload_data['kids']['kana'],
-      number_pc                : upload_data['kids']['number_pc'],
-      has_qa                   : ( upload_data['kids']['has_qa']                   === '1') ? '1' : '0',
-      is_new_contract          : ( upload_data['kids']['is_new_contract']          === '1') ? '1' : '0',
-      is_replaced_from_cj      : ( upload_data['kids']['is_replaced_from_cj']      === '1') ? '1' : '0',
-      is_replaced_from_wc      : ( upload_data['kids']['is_replaced_from_wc']      === '1') ? '1' : '0',
-      is_replaced_from_another : ( upload_data['kids']['is_replaced_from_another'] === '1') ? '1' : '0',
-      register_on              : moment().format('YYYY-MM-DD'),
-      is_registered            : 1
-    })
-    // customers
-    .then( function () {
-      return cms.model.userCustomer.register( upload_data.customers );
-    })
-    // licenses
-    .then( function () {
-      return customer.model.userLicense.register( upload_data.licenses );
-    })
-    // partners
-    .then( function () {
-      return cms.model.userPartner.register( upload_data.partners );
-    })
-    // クライアント - ユーザー数
-    .then( function () {
-      // kid , userkey が必要
-      return cms.model.userBaseInfo.registerClient({
-        kids_id             : upload_data['kids']['id'],
-        userkey             : cms.model.kids.find({'kid' : kid})[0].userkey,
-        number_client_added : upload_data['clients']['number']
-      });
-
-    })
-    // ネットワーク - クライアント数
-    .then( function () {
-      if ( upload_data['customers']['has_fenics'] === 1 ) {
-        return cms.model.userNetwork.registerFenicsAccount({
-          kids_id         : upload_data['kids']['id'],
-          fenics_key      : cms.model.kids.find({'kid' : kid})[0].fenics_key,
-          number_pc_added : upload_data['kids']['number_pc']
-        });
-      }
-    })
-    // mobiles
-    .then( function () {
-      return cms.model.userMobile.register( upload_data.mobiles );
-    })
-    .then( function () {
-      return cms.model.userBusiv.register( upload_data.busivs );
-    })
-    // all refresh
-    .then( function () {
-      cms.view.kids.refresh();
-      registerView.get('finish').get(0).showModal();
-    })
-    .catch( function (err) {
-      _alertDialog(err.message);
-    })
-    ;
-
   };
 
 
@@ -325,7 +133,9 @@
   };
 
   cms.view.regUsrs = {
-    initModule : initModule
+    initModule : initModule,
+    showError : _alertDialog,
+    showSuccess : function () { registerView.get('finish').get(0).showModal(); }
   };
 
 }( jQuery, customer ));
