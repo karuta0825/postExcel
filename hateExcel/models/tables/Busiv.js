605b22504c397bb54e44716a90d107032656ca61
karuta0825 <magune3g@gmail.com>
2018-03-18 12:33:34 +0900

    仕様変更: 正常時と同様エラー時もPromiseを返し、呼び出し側でcatchできるようにする。

--- hateExcel/models/tables/Busiv.js
+++ hateExcel/models/tables/Busiv.js
@@ -5,13 +5,16 @@ const {DbSugar} = require('../mysql/DbSugar');
  * 拠点テーブルに指定したkid_idの行を追加する
  * @param {String} kids_id [description]
  * @param {Number} base_id [description]
+ * @return {Promise<any>} [description]
+ *
+ * @throws {Promise<Exception>}
+ *
  */
-function addRow(kids_id, base_id) {
-  if (!kids_id || !base_id) { throw new Error('引数を指定してください'); }
-  return DbSugar.insert({
-    kids_id: kids_id,
-    base_id: base_id
-  }, 'make_busiv');
+async function addRow(kids_id, base_id) {
+  if (!kids_id || !base_id) {
+    return new Error('引数を指定してください');
+  }
+  return DbSugar.insert({kids_id,base_id}, 'make_busiv');
 }
 
 module.exports = {
--- hateExcel/test/sUnit/spec_Busiv.js
+++ hateExcel/test/sUnit/spec_Busiv.js
@@ -7,9 +7,36 @@ describe('Busivモジュール', () => {
 
   describe('addRowメソッド', () => {
 
-    it('kids_id, base_idを与えると、busivsテーブルに行追加');
-    it('kids_idを与えないと、エラーオブジェクトを返す');
-    it('base_idを与えないと、エラーオブジェクトを返す');
+    it('ok_kids_id, base_idを与えると、busivsテーブルに行追加')
+    // it('kids_id, base_idを与えると、busivsテーブルに行追加', () => {
+    //   return Busiv.addRow(1,99999)
+    //   .then( r => {
+    //     assert( r.affectedRows === 1 );
+    //   });
+    // });
+
+    it('引数をひとつも渡さないと、エラーオブジェクトを返す', () => {
+      return Busiv.addRow()
+      .catch( err => {
+        assert( err.message === '引数を指定してください');
+      });
+    });
+
+    it('kids_idを与えないと、エラーオブジェクトを返す', () => {
+      return Busiv.addRow(null,1)
+      .catch( err => {
+        assert( err.message === '引数を指定してください');
+      });
+    });
+
+    it('base_idを与えないと、エラーオブジェクトを返す', () => {
+      return Busiv.addRow(1)
+      .catch( err => {
+        assert( err.message === '引数を指定してください');
+      });
+    });
+
+
 
   });
 
