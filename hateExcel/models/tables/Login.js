9895e86d9aa3d0f695cf8d48fbcddaf6af9fe675
karuta0825 <magune3g@gmail.com>
2018-03-21 22:01:37 +0900

    refactor:aysync対応と引数指定を分割代入に変更

--- hateExcel/models/tables/Login.js
+++ hateExcel/models/tables/Login.js
@@ -5,13 +5,15 @@ const {DbSugar} = require('../mysql/DbSugar');
  * [authenticate description]
  * @param  {{user:String, pass:String}} data [description]
  * @return {Promise<{id:Number, uid:String, name:String, newadd:Number, is_admin:Number}| null>}      [description]
+ *
+ * @throws {Promise<Exception>}
  */
-function authenticate(data) {
-  let params = [
-    data.user,
-    data.pass
-  ];
-  return DbSugar.select(params, 'users')
+async function authenticate({user,pass}={}) {
+  if (!user || !pass) {
+    return new Error('引数を指定してください');
+  }
+
+  return DbSugar.select([user,pass], 'users')
   .then( r => {
     if ( r && r.length > 0 && r[0].newadd === 0 ) {
       delete r[0].password;
