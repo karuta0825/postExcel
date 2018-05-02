bdf34434395fa8690b3e67445d3236d6df39031c
karuta0825 <magune3g@gmail.com>
2018-05-02 15:11:14 +0900

    仕様変更:クラス化しない

--- hateExcel/models/tables/Memo.js
+++ hateExcel/models/tables/Memo.js
@@ -1,21 +1,38 @@
 
-const {DbSugar} = require('../mysql/DbSugar');
+const { DbSugar } = require('../mysql/DbSugar');
 
-class Memo extends DbSugar {
-
-  /**
-  * [addMemo description]
-  * @param {Number} options.kids_id        [description]
-  * @param {String} options.title          [description]
-  * @param {String} options.priority_id    [description]
-  * @param {String} options.message        [description]
-  * @param {Number} options.create_user_id [description]
-  * @param {Date}   options.create_on    [description]
-  */
-  static addRow(input_map) {
-    return super.insert(input_map, 'make_memo')
-  }
-
+/**
+* [addMemo description]
+* @param {Number} options.kids_id        [description]
+* @param {String} options.title          [description]
+* @param {String} options.priority_id    [description]
+* @param {String} options.message        [description]
+* @param {Number} options.create_user_id [description]
+* @param {Date}   options.create_on    [description]
+*/
+function addRow(input_map) {
+  return DbSugar.insert(input_map, 'make_memo');
 }
 
-module.exports = Memo;
+/**
+ * @param  {[type]} input_map [description]
+ * @param  {[type]} condition [description]
+ * @return {[type]}           [description]
+ */
+function update(input_map, condition) {
+  return DbSugar.update(input_map, condition, 'memos');
+}
+
+/**
+ * @param  {[type]} condition [description]
+ * @return {[type]}           [description]
+ */
+function remove(condition) {
+  return DbSugar.delete(condition, 'memos');
+}
+
+module.exports = {
+  addRow,
+  update,
+  remove,
+};
--- hateExcel/test/sUnit/spec_Memo.js
+++ hateExcel/test/sUnit/spec_Memo.js
@@ -1,58 +1,47 @@
 
 const assert = require('power-assert');
-const sinon = require('sinon');
 const Memo = require('../../models/tables/Memo');
 
 describe('Memoモジュール', () => {
-
-  describe('addMemoメソッド', () => {
-
-    it('メモ追加する', () => {
-      return Memo.addRow({
-        kids_id : '1',
-        title : 'test',
-        priority_id : '1',
-        message : 'message',
-        create_user_id: 1,
-        create_on : new Date()
-      })
-      .then( r => {
-        assert(r.affectedRows === 1);
-      })
+  describe('addRowメソッド', () => {
+    it('メモ追加する', () => Memo.addRow({
+      kids_id: '1',
+      title: 'test',
+      priority_id: '1',
+      message: 'message',
+      create_user_id: 1,
+      create_on: new Date(),
     })
-
+      .then((r) => {
+        assert(r.affectedRows === 1);
+      }));
   });
 
   describe('updateメソッド', () => {
     it('idと更新内容を受け取ると、該当行を更新する', () => {
-
       const data = {
-        title : 'chnage'
+        title: 'chnage',
       };
-      const condition = {id: 177};
-      const access = 'memos'
+      const condition = { id: 177 };
+      const access = 'memos';
 
-      return Memo.update(data,condition,access)
-      .then(r => {
-        assert(r === null);
-      })
-
+      return Memo.update(data, condition, access)
+        .then((r) => {
+          assert(r === null);
+        });
     });
   });
 
-  describe('deleteメソッド', () => {
-    it('ok_idを受け取ると、該当行のidを削除する')
+  describe('removeメソッド', () => {
+    it('ok_idを受け取ると、該当行のidを削除する');
     // it('idを受け取ると、該当行のidを削除する', () => {
-
-    //   const condition = {id:186};
+    //   const condition = { id: 221 };
     //   const access = 'memos';
 
-    //   return Memo.delete(condition, access)
-    //   .then( r => {
-    //     assert(r.affectedRows === 1);
-    //   });
-
+    //   return Memo.remove(condition, access)
+    //     .then((r) => {
+    //       assert(r.affectedRows === 1);
+    //     });
     // });
   });
-
 });
