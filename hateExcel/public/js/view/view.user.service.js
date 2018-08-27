fd776bd444a9d0892ba4130db054f31e8f82e8ac
karuta0825 <karuta0825@users.noreply.github.com>
2018-08-27 15:15:10 +0900

    update: if UI, UE.

--- hateExcel/public/js/view/view.user.service.js
+++ hateExcel/public/js/view/view.user.service.js
@@ -2,139 +2,164 @@
  * 使用ライセンス情報
  */
 
-(function ($, cms) {
-  let
-    licenseView,
-    elements = {
-      btn: {
-        edit: '.btn--edit',
-        cancel: '.btn--cancel',
-        save: '.btn--save',
-        oneline: '.btn--oneline-edit',
+( function ( $, cms ) {
+
+  var
+    licenseView
+  , elements = {
+      'btn' : {
+        'edit'    : '.btn--edit',
+        'cancel'  : '.btn--cancel',
+        'save'    : '.btn--save',
+        'oneline' : '.btn--oneline-edit',
       },
-      table: {
-        es: '.es-service-table',
-        lm: '.lm-service-table',
+      'table' : {
+        'es' : '.es-service-table',
+        'lm' : '.lm-service-table'
       },
-      select_table: '',
-      th_checkbox: 'th .mdl-checkbox',
-      td_checkbox: 'td .mdl-checkbox',
-      checkbox: '.mdl-checkbox',
-      dialog: {
-        btn: {
-          save: '.btn--dialog-save',
-          cancel: '.btn--dialog-cancel',
+      'select_table' : '',
+      'th_checkbox' : 'th .mdl-checkbox',
+      'td_checkbox' : 'td .mdl-checkbox',
+      'checkbox' : '.mdl-checkbox',
+      'dialog' : {
+        'btn'     : {
+          'save'   : '.btn--dialog-save',
+          'cancel' : '.btn--dialog-cancel'
         },
-        input: '#oneline-edit-license .oneline-input',
-        oneline: '#oneline-edit-license',
-      },
-    },
-    _checked,
-    _unchecked,
-    _selected,
-    _unselected,
-    _selectVersion,
-    _getUpdateObj,
-    _cancelOnelineInput,
-    _editOnelineInput,
-    setViewInfo,
-    setChecked,
-    makeServiceTable,
-    getViewInfo,
-    cancel,
-    save,
-    onelineSave,
-    edit,
-    clear,
-    reset,
-    initModule;
+        'input'   : '#oneline-edit-license .oneline-input',
+        'oneline' : '#oneline-edit-license'
+      }
+    }
+  , _checked
+  , _unchecked
+  , _selected
+  , _unselected
+  , _selectVersion
+  , _getUpdateObj
+  , _cancelOnelineInput
+  , _editOnelineInput
+  , setViewInfo
+  , setChecked
+  , makeServiceTable
+  , getViewInfo
+  , cancel
+  , save
+  , onelineSave
+  , edit
+  , clear
+  , reset
+  , initModule
+  ;
 
-  _checked = function (row_element) {
-    const checkbox = $(row_element).find('label');
+  _checked = function ( row_element ) {
 
-    if (!checkbox.hasClass('is-checked')) {
+    var checkbox = $(row_element).find('label');
+
+    if ( !checkbox.hasClass('is-checked') ) {
       checkbox.trigger('click');
     }
+
   };
 
-  _unchecked = function (row_element) {
-    const checkbox = $(row_element).find('label');
+  _unchecked = function ( row_element ) {
 
-    if (checkbox.hasClass('is-checked')) {
-      checkbox.trigger('click');
+    var checkbox = $(row_element).find('label');
+
+    if ( checkbox.hasClass('is-checked') ) {
+        checkbox.trigger('click');
     }
+
   };
 
-  _selected = function (el) {
+  _selected = function ( el ) {
     $(el).removeClass('is-hidden');
   };
 
-  _unselected = function (el) {
+  _unselected = function ( el ) {
     $(el).addClass('is-hidden');
   };
 
-  _setChecked = function (data) {
-    var data = _.extend({}, data[0]);
+  _setChecked = function ( data ) {
+
+    var data = _.extend( {}, data[0]);
 
     delete data.kid;
 
-    _.each(data, (val, key) => {
-      if (val === 1) {
-        _checked(licenseView.get('select-table').find(`.${key}`));
-      } else {
-        _unchecked(licenseView.get('select-table').find(`.${key}`));
+    _.each( data, function ( val, key ) {
+
+      if ( val === 1 ) {
+        _checked( licenseView.get('select-table').find('.' + key) );
       }
+      else {
+        _unchecked( licenseView.get('select-table').find('.' + key) );
+      }
+
     });
+
   };
 
-  _selectVersion = function (version) {
-    if (version === 'LM') {
+  _selectVersion = function ( version ) {
+    if ( version === 'LM') {
       licenseView.get('table__es').addClass('is-hidden');
-    } else {
+    }
+    else {
       licenseView.get('table__lm').addClass('is-hidden');
     }
   };
 
   _getUpdateObj = function () {
-    let
-      result = {},
-      obj = licenseView.get('select-table').find('tbody').find('tr');
-    _.each(obj, (val, key) => {
-      result[$(val).attr('class').split(' ')[0]] = 0;
+
+    var
+      result = {}
+    , obj = licenseView.get('select-table').find('tbody').find('tr')
+    ;
+
+    _.each( obj, function ( val, key ) {
+
+      result[ $(val).attr('class').split(' ')[0] ] = 0;
+
     });
 
     return result;
+
   };
 
   _cancelOnelineInput = function () {
-    const license = cms.model.userBaseInfo.getCache().license;
-    licenseView.get('dialog__input').val(license);
-    licenseView.get('dialog__oneline').get(0).close();
+
+      var license = cms.model.userBaseInfo.getCache().license;
+      licenseView.get('dialog__input').val(license);
+      licenseView.get('dialog__oneline').get(0).close();
+
   };
 
   _editOnelineInput = function () {
+
     cancel();
     licenseView.get('dialog__oneline').get(0).showModal();
+
   };
 
-  makeServiceTable = function (data, version) {
-    var data,
-      tmpl,
-      complied;
+  makeServiceTable = function ( data, version ) {
 
-    data = { list: data };
+    var data, tmpl, complied;
 
-    if (version === 'lm') {
-      tmpl = customer.db.getHtml('template/user.service.lm.html');
-    } else {
-      tmpl = customer.db.getHtml('template/user.service.es.html');
+    data     = { list : data };
+
+    if ( version === 'lm' ) {
+      tmpl   = customer.db.getHtml('template/user.service.lm.html');
+    }
+    else {
+      tmpl   = customer.db.getHtml('template/user.service.es.html');
     }
 
-    complied = _.template(tmpl);
+    complied = _.template( tmpl );
 
-    $('#usr-service-panel').append(complied(data));
+    $('#usr-service-panel').append( complied(data) );
 
-    componentHandler.upgradeElements($('#usr-service-panel').find('table'));
+    componentHandler.upgradeElements(
+      $('#usr-service-panel').find('table')
+    );
+
   };
 
 
@@ -143,43 +168,59 @@
    * @return {Object} result - 画面のチェックが付いたサービス一覧
    */
   getViewInfo = function () {
-    let
-      result = {},
-      obj = licenseView.get('select-table').find('tbody').find('tr');
-    _.each(obj, (val, key) => {
-      if ($(val).hasClass('is-selected')) {
-        result[$(val).attr('class').split(' ')[0]] = 1;
-      } else {
-        result[$(val).attr('class').split(' ')[0]] = 0;
+
+    var
+      result = {}
+    , obj = licenseView.get('select-table').find('tbody').find('tr')
+    ;
+
+    _.each( obj, function ( val, key ) {
+
+      if ( $(val).hasClass('is-selected') ) {
+        result[ $(val).attr('class').split(' ')[0] ] = 1;
       }
+      else {
+        result[ $(val).attr('class').split(' ')[0] ] = 0;
+      }
+
     });
 
+    result.UI = result.UE;
+
     return result;
+
   };
 
   cancel = function () {
+
     // テーブル初期化
     clear();
 
     // 登録ライセンスのみ表示
-    setViewInfo(customer.model.userLicense.getCache());
+    setViewInfo( customer.model.userLicense.getCache() );
+
   };
 
   save = function () {
-    customer.model.userLicense.update(getViewInfo(), setViewInfo);
+
+    customer.model.userLicense.update( getViewInfo(), setViewInfo );
 
     cancel();
+
   };
 
   onelineSave = function () {
-    let
-      obj = _getUpdateObj(),
-      input = licenseView.get('dialog__input').val(),
-      length = input.length,
-      i;
-    for (i = 0; i < length; i += 2) {
-      if (obj.hasOwnProperty(input.substr(i, 2))) {
-        obj[input.substr(i, 2)] = 1;
+
+    var
+      obj    = _getUpdateObj()
+    , input  = licenseView.get('dialog__input').val()
+    , length = input.length
+    , i
+    ;
+
+    for ( i = 0; i < length; i+=2 ) {
+      if ( obj.hasOwnProperty( input.substr(i,2) ) ) {
+        obj[input.substr(i,2)] = 1;
       }
     }
 
@@ -189,11 +230,13 @@
     cms.model.userLicense.update(obj, setViewInfo);
 
     licenseView.get('dialog__oneline').get(0).close();
+
   };
 
   edit = function () {
+
     // モデルから現在サービスにチェックをつける
-    _setChecked(customer.model.userLicense.getCache());
+    _setChecked( customer.model.userLicense.getCache() );
 
     // チェックボックス表示
     licenseView.get('table__lm').find('tr').removeClass('is-hidden');
@@ -206,6 +249,7 @@
     licenseView.get('btn__save').removeClass('is-hidden');
     // テキスト入力ボタン
     licenseView.get('btn__oneline').addClass('is-hidden');
+
   };
 
   /**
@@ -213,49 +257,57 @@
    * @param {[type]} data
    * TODO: 引数をObjectにする　CollectionとModelを切り分けるとできる
    */
-  setViewInfo = function (data) {
-    let
-      clone = _.extend({}, data[0]),
-      version = cms.model.userBaseInfo.getCache().version,
-      oneline = '';
-    if (version === 'LM') {
-      licenseView.updateElement({ 'select-table': '.lm-service-table' });
-    } else {
-      licenseView.updateElement({ 'select-table': '.es-service-table' });
+  setViewInfo = function ( data ) {
+
+    var
+      clone = _.extend( {}, data[0] )
+    , version = cms.model.userBaseInfo.getCache().version
+    , oneline = ''
+    ;
+
+    if ( version === 'LM' ) {
+      licenseView.updateElement({ 'select-table' : '.lm-service-table' } );
+    }
+    else {
+      licenseView.updateElement({ 'select-table' : '.es-service-table' } );
     }
 
     delete clone.kid;
 
     // 表示テーブルの選択
-    _selectVersion(version);
+    _selectVersion( version );
 
     // 利用サービスのみ表示
-    _.each(clone, (val, key) => {
-      if (val === 0) {
-        _unselected(licenseView.get('select-table').find(`.${key}`));
+    _.each( clone, function ( val, key ) {
+
+      if ( val === 0 ) {
+        _unselected( licenseView.get('select-table').find('.' + key) );
       }
 
-      if (val === 1) {
-        _selected(licenseView.get('select-table').find(`.${key}`));
+      if ( val === 1 ) {
+        _selected( licenseView.get('select-table').find('.' + key) );
         oneline += key;
       }
+
     });
 
     // テキスト入力画面の設定
     licenseView.get('dialog__input').val(oneline);
+
   };
 
   clear = function () {
-    const data = customer.model.userLicense.getCache();
+
+    var data = customer.model.userLicense.getCache();
 
     // LMES両方テーブル表示
     licenseView.get('table__es').removeClass('is-hidden');
     licenseView.get('table__lm').removeClass('is-hidden');
 
-    _.each(data[0], (val, key) => {
+    _.each( data[0], function ( val, key ) {
       // 再表示
-      _selected(licenseView.get('select-table').find(`.${key}`));
-      _unchecked(licenseView.get('select-table').find(`.${key}`));
+      _selected( licenseView.get('select-table').find('.' + key));
+      _unchecked( licenseView.get('select-table').find('.' + key));
     });
 
     // チェックボックスの非表示
@@ -266,35 +318,39 @@
     licenseView.get('btn__cancel').addClass('is-hidden');
     licenseView.get('btn__save').addClass('is-hidden');
     licenseView.get('btn__oneline').removeClass('is-hidden');
+
   };
 
   initModule = function () {
+
     // テーブル生成
-    makeServiceTable(cms.model.services.find({ version: 'LM', is_setup_info: 0 }), 'lm');
-    makeServiceTable(cms.model.services.find({ version: 'ES', is_setup_info: 0 }), 'es');
+    makeServiceTable( cms.model.services.find({ 'version' : 'LM', 'is_setup_info' : 0 }), 'lm' );
+    makeServiceTable( cms.model.services.find({ 'version' : 'ES', 'is_setup_info' : 0 }), 'es' );
 
     licenseView = new Controller('#usr-service-panel');
-    licenseView.initElement(elements);
+    licenseView.initElement( elements );
 
     licenseView.addListener({
-      'click btn__edit': edit,
-      'click btn__cancel': cancel,
-      'click btn__save': save,
-      'click btn__oneline': _editOnelineInput,
-      'click dialog__btn__cancel': _cancelOnelineInput,
-      'click dialog__btn__save': onelineSave,
+      'click btn__edit'    : edit,
+      'click btn__cancel'  : cancel,
+      'click btn__save'    : save,
+      'click btn__oneline' : _editOnelineInput,
+      'click dialog__btn__cancel' : _cancelOnelineInput,
+      'click dialog__btn__save' : onelineSave
     });
 
     licenseView.get('checkbox').addClass('is-hidden');
+
   };
 
   // to public
   cms.view.userService = {
-    initModule,
-    makeServiceTable,
-    clear,
-    setViewInfo,
-    getViewInfo,
-    get() { return licenseView; },
-  };
-}(jQuery, customer));
+    initModule : initModule,
+    makeServiceTable : makeServiceTable,
+    clear : clear,
+    setViewInfo : setViewInfo,
+    getViewInfo : getViewInfo,
+    get : function () { return licenseView; }
+  }
+
+}( jQuery, customer ))
